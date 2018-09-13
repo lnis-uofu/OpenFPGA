7261a53f3ca9c25d8d0b1ddc5aa645f59c95b0f2
Xifan Tang <u6018939@lnissrv4.eng.utah.edu>
2018-09-13 15:48:10 -0600

    update docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+ import os
+ import sys
+ import shlex
 # sys.path.insert(0, os.path.abspath('.'))
+
+ import sphinx_rtd_theme
+
+ html_theme = "sphinx_rtd_theme"
+ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # -- Project information -----------------------------------------------------
