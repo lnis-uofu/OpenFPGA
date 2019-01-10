9c8444da437c14432ef8d12f6b43ba2893371141
Baudouin Chauviere <baudouin.chauviere@utah.edu>
2019-01-10 00:06:20 -0700

    update of the examples supplied to get the right paths

--- examples/example_1.sh
+++ examples/example_1.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Example of how to run vpr
 
+# The paths need to be absolute hence we modify a keyword with PWD
+
+sed "s:OPENFPGAPATH:${PWD}/..:g" example_1_template.xml > example_1.xml
+
 # Pack, place, and route a heterogeneous FPGA
 # Packing uses the AAPack algorithm
 ../vpr7_x2p/vpr/vpr ./example_1.xml ./example_1.blif --full_stats --nodisp --route_chan_width 30 --fpga_spice --fpga_spice_rename_illegal_port --fpga_spice_dir ./spice_test_example_1 --fpga_spice_print_top_testbench --fpga_spice_print_grid_testbench --fpga_spice_print_cb_testbench --fpga_spice_print_sb_testbench --fpga_spice_print_lut_testbench --fpga_spice_print_hardlogic_testbench --fpga_spice_print_pb_mux_testbench --fpga_spice_print_cb_mux_testbench --fpga_spice_print_sb_mux_testbench --fpga_verilog --fpga_verilog_dir ./verilog_test_example_1
--- examples/example_2.sh
+++ examples/example_2.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Example of how to run vpr
 
+
+# The paths need to be absolute hence we modify a keyword with PWD
+
+sed "s:OPENFPGAPATH:${PWD}/..:g" example_2_template.xml > example_2.xml
+
 # Pack, place, and route a heterogeneous FPGA
 # Packing uses the AAPack algorithm
 ../vpr7_x2p/vpr/vpr ./example_2.xml ./example_2.blif --full_stats --nodisp --route_chan_width 100 --fpga_spice --fpga_spice_rename_illegal_port --fpga_spice_dir ./spice_test_example_2 --fpga_spice_print_top_testbench --fpga_spice_print_grid_testbench --fpga_spice_print_cb_testbench --fpga_spice_print_sb_testbench --fpga_spice_print_lut_testbench --fpga_spice_print_hardlogic_testbench --fpga_spice_print_pb_mux_testbench --fpga_spice_print_cb_mux_testbench --fpga_spice_print_sb_mux_testbench --fpga_verilog --fpga_verilog_dir ./verilog_test_example_2
