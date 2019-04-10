e2e4a9287ddd3f88ee6d436fa4b7e616bcd20390
tangxifan <tangxifan@gmail.com>
2019-04-10 16:10:19 +0800

    keep bug fixing in Makefiles and remove parallism in Make

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,10 +7,10 @@ $SPACER
 
 start_section "OpenFPGA.build" "${GREEN}Building..${NC}"
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  make -j1
+  make
 else 
 # For linux, we enable full package compilation
-  make -j8
+  make
 fi
 end_section "OpenFPGA.build"
 
