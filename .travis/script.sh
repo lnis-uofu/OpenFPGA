f5b6ee6adfa24130bcf8a6c70708e936d2f129c0
tangxifan <tangxifan@gmail.com>
2019-06-07 22:19:11 -0600

    update travis configuration and clean up repository

--- .travis/script.sh
+++ .travis/script.sh
@@ -10,7 +10,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   #make
   mkdir build
   cd build
-  cmake ..
+  cmake .. -DCMAKE_BUILD_TYPE=debug -ENABLE_VPR_GRAPHICS=off 
   make -j2
 else 
 # For linux, we enable full package compilation
