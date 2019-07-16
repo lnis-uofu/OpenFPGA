d6dfc29508b7bfa9c7edf504394da4e3f105e608
tangxifan <tangxifan@gmail.com>
2019-07-16 17:00:09 -0600

    Now we use the ace from  VTR

--- ace2/SRC/io_ace.c
+++ ace2/SRC/io_ace.c
@@ -71,7 +71,7 @@ void ace_io_print_activity(Abc_Ntk_t * ntk, FILE * fp) {
 
 		default:
 			//printf("Unkown Type: %d\n", Abc_ObjType(obj));
-			//assert(0);
+			//VTR_ASSERT(0);
 			break;
 		}
 
@@ -237,10 +237,10 @@ int ace_io_read_activity(Abc_Ntk_t * ntk, FILE * in_file_desc,
 			printf("Cannot open input file\n");
 			error = ACE_ERROR;
 		} else {
-			assert(p >= 0.0 && p <= 1.0);
-			assert(d >= 0.0 && d <= 1.0);
-			assert(d <= 2.0 * p);
-			assert(d <= 2.0 * (1.0 - p));
+			VTR_ASSERT(p >= 0.0 && p <= 1.0);
+			VTR_ASSERT(d >= 0.0 && d <= 1.0);
+			VTR_ASSERT(d <= 2.0 * p);
+			VTR_ASSERT(d <= 2.0 * (1.0 - p));
 
 			Abc_NtkForEachPi(ntk, obj_ptr, i)
 			{
@@ -285,8 +285,8 @@ int ace_io_read_activity(Abc_Ntk_t * ntk, FILE * in_file_desc,
 				}
 				pi_obj_ptr = Abc_NtkObj(ntk, pi_obj_id);
 
-				assert(static_prob >= 0.0 && static_prob <= 1.0);
-				assert(switch_prob >= 0.0 && switch_prob <= 1.0);
+				VTR_ASSERT(static_prob >= 0.0 && static_prob <= 1.0);
+				VTR_ASSERT(switch_prob >= 0.0 && switch_prob <= 1.0);
 
 				info = Ace_ObjInfo(pi_obj_ptr);
 				info->static_prob = static_prob;
@@ -338,7 +338,7 @@ int ace_io_read_activity(Abc_Ntk_t * ntk, FILE * in_file_desc,
 					error = ACE_ERROR;
 					break;
 				}
-				assert(strlen(vector) == num_Pi);
+				VTR_ASSERT(strlen(vector) == num_Pi);
 
 				if (num_vec == 0) {
 					Abc_NtkForEachPi(ntk, obj_ptr, i)
@@ -373,7 +373,7 @@ int ace_io_read_activity(Abc_Ntk_t * ntk, FILE * in_file_desc,
 			if (!error) {
 				Abc_NtkForEachPi(ntk, obj_ptr, i)
 				{
-                    assert(num_vec > 0);
+                    VTR_ASSERT(num_vec > 0);
 
 					info = Ace_ObjInfo(obj_ptr);
 					info->static_prob = (double) high[i] / (double) num_vec;
