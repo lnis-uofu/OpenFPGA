ef4d15df4e0bcf7b50d82555d3612cf473364d8b
tangxifan <tangxifan@gmail.com>
2019-08-13 13:37:35 -0600

    reorganize the libarchfpga repository

--- vpr7_x2p/vpr/SRC/ctags_vpr_src.sh
+++ vpr7_x2p/vpr/SRC/ctags_vpr_src.sh
@@ -1,2 +1,2 @@
 rm tags
-ctags -R shell_main.c main.c ./* ../../libarchfpga/SRC/include/*.[ch] ../../libarchfpga/SRC/fpga_spice_include/*.[ch] ../../libarchfpga/SRC/*.[ch] ../../pcre/SRC/*.[ch]
+ctags -R shell_main.c main.c ./* ../../libarchfpga/SRC/*.[ch] ../../pcre/SRC/*.[ch]
