10eba0f78cd03d41961a874c0209254a457ecbfd
Ganesh Gore <goreganesh007@gmail.com>
2019-09-13 23:31:23 -0600

    Updated script.sh with new paramters

--- .travis/script.sh
+++ .travis/script.sh
@@ -18,5 +18,5 @@ end_section "OpenFPGA.build"
 
 start_section "OpenFPGA.TaskTun" "${GREEN}..Running_Regression..${NC}"
 cd -
-python3 openfpga_flow/scripts/run_fpga_task.py blif_vpr_flow --exit_on_fail
+python3 openfpga_flow/scripts/run_fpga_task.py blif_vpr_flow
 end_section "OpenFPGA.TaskTun"
