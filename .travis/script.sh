c80cbfe8c9464eb60a63134c9e5af1a1a3733325
Ganesh Gore <goreganesh007@gmail.com>
2019-11-02 17:55:36 -0600

    Removed files after regression test

--- .travis/script.sh
+++ .travis/script.sh
@@ -19,19 +19,24 @@ end_section "OpenFPGA.build"
 start_section "OpenFPGA.TaskTun" "${GREEN}..Running_Regression..${NC}"
 cd -
 echo -e "Testing single-mode architectures";
-python3 openfpga_flow/scripts/run_fpga_task.py single_mode --debug --show_thread_logs
+python3 openfpga_flow/scripts/run_fpga_task.py single_mode
+python3 openfpga_flow/scripts/run_fpga_task.py single_mode --remove_run_dir all
 #python3 openfpga_flow/scripts/run_fpga_task.py s298
 
 echo -e "Testing multi-mode architectures";
-python3 openfpga_flow/scripts/run_fpga_task.py blif_vpr_flow --maxthreads 4
+python3 openfpga_flow/scripts/run_fpga_task.py blif_vpr_flow --maxthreads 2
+python3 openfpga_flow/scripts/run_fpga_task.py blif_vpr_flow --remove_run_dir all
 
 echo -e "Testing compact routing techniques";
 python3 openfpga_flow/scripts/run_fpga_task.py compact_routing
+python3 openfpga_flow/scripts/run_fpga_task.py compact_routing --remove_run_dir all
 
 echo -e "Testing tileable architectures";
 python3 openfpga_flow/scripts/run_fpga_task.py tileable_routing
+python3 openfpga_flow/scripts/run_fpga_task.py tileable_routing --remove_run_dir all
 
 echo -e "Testing Verilog generation with explicit port mapping ";
 python3 openfpga_flow/scripts/run_fpga_task.py explicit_verilog
+python3 openfpga_flow/scripts/run_fpga_task.py explicit_verilog --remove_run_dir all
 
 end_section "OpenFPGA.TaskTun"
