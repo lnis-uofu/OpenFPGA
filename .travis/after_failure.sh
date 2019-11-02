d14be73825da01c5e107a6e68ad794c4cee91d2f
Ganesh Gore <goreganesh007@gmail.com>
2019-11-02 17:51:30 -0600

    Added backup option in travis failure

--- .travis/after_failure.sh
+++ .travis/after_failure.sh
@@ -9,4 +9,7 @@ travis_fold end after_failure.1
 
 start_section "failure.tail" "${RED}Failure output...${NC}"
 tail -n 1000 output.log
+echo "Failed uploading files to LNIS Server"
+scp -qCr $TRAVIS_BUILD_DIR/openfpga_flow/tasks/ u1249762@lab1-1.eng.utah.edu:/var/tmp/travis_bc/$TRAVIS_JOB_ID/
+scp output.log u1249762@lab1-1.eng.utah.edu:/var/tmp/travis_bc/$TRAVIS_JOB_ID/
 end_section "failure.tail"
