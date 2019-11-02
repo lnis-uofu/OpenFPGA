5c8ca81645c2db583ffa6a7f61e51dff1f4d1f2a
Ganesh Gore <goreganesh007@gmail.com>
2019-11-01 23:06:33 -0600

    Travis failure debug

--- .travis/script.sh
+++ .travis/script.sh
@@ -19,19 +19,19 @@ end_section "OpenFPGA.build"
 start_section "OpenFPGA.TaskTun" "${GREEN}..Running_Regression..${NC}"
 cd -
 echo -e "Testing single-mode architectures";
-python3 openfpga_flow/scripts/run_fpga_task.py single_mode 
-#python3 openfpga_flow/scripts/run_fpga_task.py s298 
+python3 openfpga_flow/scripts/run_fpga_task.py single_mode --debug
+#python3 openfpga_flow/scripts/run_fpga_task.py s298
 
-echo -e "Testing multi-mode architectures";
-python3 openfpga_flow/scripts/run_fpga_task.py blif_vpr_flow --maxthreads 4
+# echo -e "Testing multi-mode architectures";
+# python3 openfpga_flow/scripts/run_fpga_task.py blif_vpr_flow --maxthreads 4
 
-echo -e "Testing compact routing techniques";
-python3 openfpga_flow/scripts/run_fpga_task.py compact_routing 
+# echo -e "Testing compact routing techniques";
+# python3 openfpga_flow/scripts/run_fpga_task.py compact_routing
 
-echo -e "Testing tileable architectures";
-python3 openfpga_flow/scripts/run_fpga_task.py tileable_routing 
+# echo -e "Testing tileable architectures";
+# python3 openfpga_flow/scripts/run_fpga_task.py tileable_routing
 
-echo -e "Testing Verilog generation with explicit port mapping ";
-python3 openfpga_flow/scripts/run_fpga_task.py explicit_verilog 
+# echo -e "Testing Verilog generation with explicit port mapping ";
+# python3 openfpga_flow/scripts/run_fpga_task.py explicit_verilog
 
 end_section "OpenFPGA.TaskTun"
