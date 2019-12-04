322228de43023fbf9b19e6d20bfb8bb146b4ac0f
tangxifan <tangxifan@gmail.com>
2019-12-04 16:02:43 -0700

    remove legacy codes in FPGA-Verilog

--- vpr7_x2p/vpr/SRC/fpga_x2p/verilog/verilog_submodules.h
+++ vpr7_x2p/vpr/SRC/fpga_x2p/verilog/verilog_submodules.h
@@ -13,14 +13,4 @@ void print_verilog_submodules(ModuleManager& module_manager,
                               const t_arch& Arch, 
                               const t_syn_verilog_opts& fpga_verilog_opts);
 
-/* TODO: Old function to be deprecated */
-void dump_verilog_submodules(ModuleManager& module_manager, 
-                             const MuxLibrary& mux_lib,
-                             t_sram_orgz_info* cur_sram_orgz_info,
-                             char* verilog_dir, 
-                             char* submodule_dir, 
-                             t_arch Arch, 
-                             t_det_routing_arch* routing_arch,
-                             t_syn_verilog_opts fpga_verilog_opts);
-
 #endif
