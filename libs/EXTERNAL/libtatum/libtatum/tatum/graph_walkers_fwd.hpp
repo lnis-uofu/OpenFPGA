60cbcf910464ee4cd7506d0d8014528912d0b822
tangxifan <tangxifan@gmail.com>
2020-01-03 22:42:17 -0500

    add missing tatum

---
+++ libs/EXTERNAL/libtatum/libtatum/tatum/graph_walkers_fwd.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace tatum {
+
+class SerialWalker;
+
+class ParallelLevelizedWalker;
+
+///The default parallel graph walker
+using ParallelWalker = ParallelLevelizedWalker;
+
+} //namespace
+
