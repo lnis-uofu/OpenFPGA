a99835e1c121bc7052c6201f610046acb4a5d7bc
tangxifan <tangxifan@gmail.com>
2020-02-01 21:53:31 -0700

    rr_graph area estimator adopted RRGraph object

--- vpr/src/route/rr_graph_util.h
+++ vpr/src/route/rr_graph_util.h
@@ -1,8 +1,8 @@
 #ifndef RR_GRAPH_UTIL_H
 #define RR_GRAPH_UTIL_H
 
-int seg_index_of_cblock(t_rr_type from_rr_type, int to_node);
+int seg_index_of_cblock(t_rr_type from_rr_type, const RRNodeId& to_node);
 
-int seg_index_of_sblock(int from_node, int to_node);
+int seg_index_of_sblock(const RRNodeId& from_node, const RRNodeId& to_node);
 
 #endif
