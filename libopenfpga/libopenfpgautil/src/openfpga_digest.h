622c7826d1f48cf8a457aeda1dadd11ce05fda7f
tangxifan <tangxifan@gmail.com>
2020-02-15 15:03:00 -0700

    start transplanting fpga_verilog

--- libopenfpga/libopenfpgautil/src/openfpga_digest.h
+++ libopenfpga/libopenfpgautil/src/openfpga_digest.h
@@ -17,6 +17,14 @@ bool valid_file_stream(std::fstream& fp);
 void check_file_stream(const char* fname, 
                        std::fstream& fp);
 
+std::string format_dir_path(const std::string& dir_path_to_format);
+
+std::string find_path_file_name(const std::string& file_name);
+
+std::string find_path_dir_name(const std::string& file_name);
+
+bool create_dir_path(const char* dir_path);
+
 } /* namespace openfpga ends */
 
 #endif
---
+++ openfpga/src/fpga_verilog/verilog_api.h
@@ -0,0 +1,36 @@
+#ifndef VERILOG_API_H 
+#define VERILOG_API_H 
+
+/********************************************************************
+ * Include header files that are required by function declaration
+ *******************************************************************/
+
+#include <string>
+#include <vector>
+#include "vpr_types.h"
+#include "mux_library.h"
+#include "circuit_library.h"
+#include "device_grid.h"
+#include "device_rr_gsb.h"
+#include "module_manager.h"
+
+/********************************************************************
+ * Function declaration
+ *******************************************************************/
+
+/* begin namespace openfpga */
+namespace openfpga {
+
+void fabric_verilog(ModuleManager& module_manager,
+                    const CircuitLibrary& circuit_lib,
+                    const MuxLibrary& mux_lib,
+                    const DeviceGrid& grids, 
+                    const DeviceRRGSB& device_rr_gsb,
+                    const std::string& output_directory,
+                    const bool& compress_routing,
+                    const bool& dump_explict_verilog,
+                    const bool& verbose);
+
+} /* end namespace openfpga */
+
+#endif
