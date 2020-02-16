a88c4bc954f38edef0fb29ee7e753b357cab47dd
tangxifan <tangxifan@gmail.com>
2020-02-16 12:21:59 -0700

    add decode utils to libopenfpga and adapt local decoder writer in Verilog

---
+++ libopenfpga/libopenfpgautil/src/openfpga_decode.h
@@ -0,0 +1,23 @@
+#ifndef OPENFPGA_DECODE_H
+#define OPENFPGA_DECODE_H
+
+/********************************************************************
+ * Include header files that are required by function declaration
+ *******************************************************************/
+#include <vector>
+
+/********************************************************************
+ * Function declaration
+ *******************************************************************/
+/* namespace openfpga begins */
+namespace openfpga {
+
+std::vector<size_t> ito1hot_vec(const size_t& in_int,
+                                const size_t& bin_len);
+
+std::vector<size_t> itobin_vec(const size_t& in_int,
+                               const size_t& bin_len);
+
+} /* namespace openfpga ends */
+
+#endif
---
+++ openfpga/src/fpga_verilog/verilog_decoders.h
@@ -0,0 +1,32 @@
+#ifndef VERILOG_DECODERS_H
+#define VERILOG_DECODERS_H
+
+/********************************************************************
+ * Include header files that are required by function declaration
+ *******************************************************************/
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "circuit_library.h"
+#include "mux_graph.h"
+#include "mux_library.h"
+#include "module_manager.h"
+
+/********************************************************************
+ * Function declaration
+ *******************************************************************/
+
+/* begin namespace openfpga */
+namespace openfpga {
+
+void print_verilog_submodule_mux_local_decoders(ModuleManager& module_manager,
+                                                std::vector<std::string>& netlist_names,
+                                                const MuxLibrary& mux_lib,
+                                                const CircuitLibrary& circuit_lib,
+                                                const std::string& verilog_dir,
+                                                const std::string& submodule_dir);
+
+} /* end namespace openfpga */
+
+#endif
