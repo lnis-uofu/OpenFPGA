cf34339e96cfbd20c8f93c5e11c2966dfb9142d9
tangxifan <tangxifan@gmail.com>
2020-02-16 11:57:19 -0700

    adapt essential gates for submodule generation

---
+++ openfpga/src/fpga_verilog/verilog_essential_gates.h
@@ -0,0 +1,25 @@
+#ifndef VERILOG_ESSENTIAL_GATES_H
+#define VERILOG_ESSENTIAL_GATES_H
+
+/********************************************************************
+ * Include header files that are required by function declaration
+ *******************************************************************/
+#include <string>
+#include "circuit_library.h"
+
+/********************************************************************
+ * Function declaration
+ *******************************************************************/
+
+/* begin namespace openfpga */
+namespace openfpga {
+
+void print_verilog_submodule_essentials(ModuleManager& module_manager, 
+                                        std::vector<std::string>& netlist_names,
+                                        const std::string& verilog_dir, 
+                                        const std::string& submodule_dir,
+                                        const CircuitLibrary& circuit_lib);
+
+} /* end namespace openfpga */
+
+#endif
