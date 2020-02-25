2d86a02358278e85c6d2cc4cd550ffd7927dfe95
tangxifan <tangxifan@gmail.com>
2020-02-25 12:45:13 -0700

    refactored LUT bitstream generation to use vtr logic

--- libs/libvtrutil/src/vtr_logic.h
+++ libs/libvtrutil/src/vtr_logic.h
@@ -1,14 +1,19 @@
 #ifndef VTR_LOGIC_H
 #define VTR_LOGIC_H
 
+#include <array>
+
 namespace vtr {
 
 enum class LogicValue {
     FALSE = 0,
     TRUE = 1,
     DONT_CARE = 2,
-    UNKOWN = 3
+    UNKOWN = 3,
+    NUM_LOGIC_VALUE_TYPES
 };
+
+constexpr std::array<const char*, size_t(LogicValue::NUM_LOGIC_VALUE_TYPES)> LOGIC_VALUE_STRING = {{"false", "true", "don't care", "unknown"}};
 
 } // namespace vtr
 
--- openfpga/src/utils/lut_utils.h
+++ openfpga/src/utils/lut_utils.h
@@ -6,7 +6,11 @@
  *******************************************************************/
 #include <string>
 #include <vector>
+#include <map>
 #include "atom_netlist.h"
+#include "mux_graph.h"
+#include "physical_types.h"
+#include "vpr_device_annotation.h"
 
 /********************************************************************
  * Function declaration
@@ -19,6 +23,21 @@ AtomNetlist::TruthTable lut_truth_table_adaption(const AtomNetlist::TruthTable& 
                                                  const std::vector<int>& rotated_pin_map);
 
 std::vector<std::string> truth_table_to_string(const AtomNetlist::TruthTable& tt);
+
+AtomNetlist::TruthTable build_post_routing_wired_lut_truth_table(const size_t& lut_size, 
+                                                                 const size_t& wire_input_id);
+
+AtomNetlist::TruthTable adapt_truth_table_for_frac_lut(const size_t& lut_frac_level,
+                                                       const size_t& lut_output_mask,
+                                                       const AtomNetlist::TruthTable& truth_table);
+
+bool lut_truth_table_use_on_set(const AtomNetlist::TruthTable& truth_table);
+
+std::vector<bool> build_frac_lut_bitstream(const CircuitLibrary& circuit_lib,
+                                           const MuxGraph& lut_mux_graph, 
+                                           const VprDeviceAnnotation& device_annotation, 
+                                           const std::map<t_pb_graph_pin*, AtomNetlist::TruthTable>& truth_tables,
+                                           const size_t& default_sram_bit_value);
 
 } /* end namespace openfpga */
 
