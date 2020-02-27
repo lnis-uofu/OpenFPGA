bb671acac3bf1d8aaad193a2aa9c50a1e1845d24
tangxifan <tangxifan@gmail.com>
2020-02-26 20:58:16 -0700

    add formal random Verilog testbench generation

---
+++ openfpga/src/fpga_verilog/verilog_formal_random_top_testbench.h
@@ -0,0 +1,26 @@
+#ifndef VERILOG_FORMAL_RANDOM_TOP_TESTBENCH
+#define VERILOG_FORMAL_RANDOM_TOP_TESTBENCH
+
+/********************************************************************
+ * Include header files that are required by function declaration
+ *******************************************************************/
+#include <string>
+#include "vpr_context.h"
+#include "simulation_setting.h"
+
+/********************************************************************
+ * Function declaration
+ *******************************************************************/
+
+/* begin namespace openfpga */
+namespace openfpga {
+
+void print_verilog_random_top_testbench(const std::string& circuit_name,
+                                        const std::string& verilog_fname,
+                                        const std::string& verilog_dir,
+                                        const AtomContext& atom_ctx,
+                                        const SimulationSetting& simulation_parameters);
+
+} /* end namespace openfpga */
+
+#endif
