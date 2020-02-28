d136ac236f6f831b4f0167e6023389d856085a1a
tangxifan <tangxifan@gmail.com>
2020-02-27 19:39:57 -0700

    adapt sdc memory utils

---
+++ openfpga/src/fpga_sdc/sdc_memory_utils.h
@@ -0,0 +1,25 @@
+#ifndef SDC_MEMORY_UTILS_H
+#define SDC_MEMORY_UTILS_H
+
+/********************************************************************
+ * Include header files that are required by function declaration
+ *******************************************************************/
+#include <fstream>
+#include <string>
+#include "module_manager.h"
+
+/********************************************************************
+ * Function declaration
+ *******************************************************************/
+
+/* begin namespace openfpga */
+namespace openfpga {
+
+void rec_print_pnr_sdc_disable_configurable_memory_module_output(std::fstream& fp, 
+                                                                 const ModuleManager& module_manager, 
+                                                                 const ModuleId& parent_module,
+                                                                 const std::string& parent_module_path);
+
+} /* end namespace openfpga */
+
+#endif
