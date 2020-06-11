60dd37e08603c5a863651a35ca4aac2037885dcc
tangxifan <tangxifan@gmail.com>
2020-06-11 12:05:12 -0600

    remove simulation settings from openfpga arch XML
    update travis to split CI tests
    
    fix errors in travis configuration
    
    fixing travis errors in scripts
    
    keep fixing travis
    
    fix travis on build.sh
    
    bug fixing in travis CI
    
    bug fix in travis regression test run
    
    fixing bugs in the travis scripts
    
    bug fix in travis script: remove common.sh in regression test call
    
    keep bug fixing in travis

--- .travis/openfpga_vpr8_reg_test.sh
+++ .travis/openfpga_vpr8_reg_test.sh
@@ -1,46 +1,9 @@
 #!/bin/bash
 
-source .travis/common.sh
 set -e
 
-start_section "OpenFPGA.build" "${GREEN}Building..${NC}"
-mkdir build
-cd build
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  cmake .. -DCMAKE_BUILD_TYPE=debug -DENABLE_VPR_GRAPHICS=off
-else
-  cmake .. -DCMAKE_BUILD_TYPE=debug
-fi
- make -j16
-end_section "OpenFPGA.build"
-
-
 start_section "OpenFPGA.TaskTun" "${GREEN}..Running_Regression..${NC}"
-cd -
-
-###############################################
-# OpenFPGA with VPR7
-# TO BE DEPRECATED
-##############################################
-echo -e "Testing single-mode architectures";
-python3 openfpga_flow/scripts/run_fpga_task.py single_mode --debug --show_thread_logs
-#python3 openfpga_flow/scripts/run_fpga_task.py s298 
-
-echo -e "Testing multi-mode architectures";
-python3 openfpga_flow/scripts/run_fpga_task.py multi_mode --maxthreads 4 --debug --show_thread_logs
-
-echo -e "Testing compact routing techniques";
-python3 openfpga_flow/scripts/run_fpga_task.py compact_routing --debug --show_thread_logs
-
-echo -e "Testing tileable architectures";
-python3 openfpga_flow/scripts/run_fpga_task.py tileable_routing --debug --show_thread_logs
-
-echo -e "Testing Verilog generation with explicit port mapping ";
-python3 openfpga_flow/scripts/run_fpga_task.py explicit_verilog --debug --show_thread_logs
-
-echo -e "Testing Verilog generation with grid pin duplication ";
-python3 openfpga_flow/scripts/run_fpga_task.py duplicate_grid_pin --debug --show_thread_logs
+cd ${TRAVIS_BUILD_DIR}
 
 ###############################################
 # OpenFPGA Shell with VPR8
