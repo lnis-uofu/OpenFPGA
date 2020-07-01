4b426d366c9b5a94ad07d14f5f957e7aee2e265e
CHARAS SAMY <u6030277@lnissrv4.eng.utah.edu>
2020-07-01 13:24:47 -0600

    .

--- openfpga_flow/uvm/seq_generator/seq_gen.py
+++ openfpga_flow/uvm/seq_generator/seq_gen.py
@@ -5,14 +5,33 @@ import fileinput
 import sys
 import re
 
-#Command example : python seq_gen.py
+#Command example : python seq_gen.py -s simulation_deck.ini -b prog.bit
 
-#Adapt the path here to the testbench bs_agent directory you're using
-#For the sequence
-seq_lib_path="../tb/bs_agent/seq_lib/"
-name="prog_seq_generated.sv"
-sequence = seq_lib_path + name
-filename = "../../SRC/and2_autocheck_top_tb.v"
+parser = argparse.ArgumentParser(description="create new python script from a template.")
+parser.add_argument(
+	"-s",
+	"--simulation_deck",
+	help="the name of the simulation deck file in which parameters are stored",
+	default="test"
+)
+parser.add_argument(
+	"-b",
+	"--prog",
+	help="The name of the programmation file in which the fpga config is stored",
+	default="test"
+)
+args=parser.parse_args()
+sim_file = args.simulation_deck
+prog_file = args.prog
+
+gpio_start = "GPIO_WIDTH = "
+addr_start = "ADDR_WIDTH = "
+io_start = "IO = "
+#Source file to adapt the testbench parameters ( scalability )
+uvm_pkg_template = "uvm_pkg.py"
+uvm_pkg = "./uvm_tb.sv"
+
+#Template and param to build uvm seq
 template = '''`ifndef "prog_seq_gen"
 `define "prog_seq_gen"
 
@@ -40,58 +59,23 @@ endfunction // new
 task prog_seq_gen::body();
 '''
 
-#Source file to adapt the testbench parameters ( scalability )
-uvm_pkg_template = "uvm_pkg.py"
-uvm_pkg = "../tb/uvm_tb.sv"
+sequence = "./seq.sv"
+cycle_task ="		prog_cycle_task("
 
-#String used to extract parameters from the verilog testbench
-start1 = "wire [0:"
-end1 = "] gfpga_pad_iopad_pad;"
-start2= "reg  [0:"
-end2= "] address;"
-assign_statement = "assign gfpga_pad_iopad_pad["
-end_assign = "] ="
-prog_statement = "		prog_cycle_task("
-
-# Extract function takes out the programmation out of an autochecking testbench generated with yosys/vpr
-# Then write the programming sequence to the sequence placeholder
-def extract(file,startExp,endExp):
-	results = io_seeker()
-	ie_value = results[0]
-	gpio_width = results[1]
-	index = 0                                
-	with open (filename, 'rt') as myfile:
-		with open(sequence, "w") as output:
-			output.write(template)    
-			for line in myfile:
-				if startExp in line:
-					index = 1
-					addr_value = line[line.find(prog_statement)+len(prog_statement):line.rfind(",")]
-					data_value = line[line.find(",")+len(","):line.rfind(");")]
-				if endExp in line:
-					if index ==1:
-						index = 0
-						output.write("		`uvm_do_with(req,{req.address == " + addr_value + "; req.data_in ==" + data_value + "; req.IE == " + str(gpio_width) + "'b" + ie_value + ";})")
-						output.write("\nendtask;")
-						output.write("\n\n`endif")
-				if index !=0:                   
-					output.write(line)
-
-
-# Get GPIO_WIDTH & ADDR_WIDTH from verilog TB.
 
 def extract_parameters(file):
-	global gpio_width
 	with open (file, 'rt') as myfile:
 		for line in myfile:
-			if end1 in line:
-				gpio_width = line[line.find(start1)+len(start1):line.rfind(end1)]
-			if end2 in line:
-				addr_width = line[line.find(start2)+len(start2):line.rfind(end2)]
+			if gpio_start in line:
+				gpio_width = line[line.find(gpio_start)+len(gpio_start):line.rfind("")]
+			if addr_start in line:
+				addr_width = line[line.find(addr_start)+len(addr_start):line.rfind("")]
+			if io_start in line:
+				io_value = line[line.find(io_start)+len(io_start):line.rfind("")]
 	print("parameter gpio_width =" + gpio_width)
 	print("parameter addr_width =" + addr_width)
-	return gpio_width
 	pkg_generator(gpio_width,addr_width)
+	extract(prog_file,gpio_width,io_value,addr_width)
 
 # Modify parameters in the UVM testbench
 def pkg_generator(gpio_width,addr_width):
@@ -107,30 +91,18 @@ def pkg_generator(gpio_width,addr_width):
     with open(uvm_pkg, "w") as f:
 	f.write(result)
 
-
-
-
-def io_seeker():
-	global results
-	results = [0] * 2
-	ie_value = ""
-	gpio_widt = extract_parameters(filename)
-	gpio_width = int(gpio_widt) + 1
-	IE = [0] * gpio_width
-	final_config = ""
-	with open (filename, 'rt') as myfile:
-		for line in myfile:
-			for i in range(gpio_width):
-				if assign_statement + str(i) + end_assign in line:
-					IE[i] = 1
-	for i in range(gpio_width):	
-		ie_value = ie_value + str(IE[i])
-	print(ie_value)
-	results[0] = ie_value
-	results[1] = gpio_width
-	return results
+def extract(file,gpio_width,io_value,addr_width):       
+	with open (file, 'rt') as myfile:
+		with open(sequence, "w") as output:
+			output.write(template)    
+			for line in myfile:
+					addr_value = line[line.find("")+len(""):line.rfind(" ")]
+					data_value = line[line.find(" ")+len(" "):line.rfind("")]
+					final_line = cycle_task + addr_width[:-1] +"'b" + addr_value + ", 1'b" + data_value[:-1] + ");\n"
+					output.write(final_line)
+			output.write("		`uvm_do_with(req,{req.address == " + addr_width[:-1] +"'b" + addr_value + "; req.data_in ==" + data_value[:-1] + "; req.IE == " + gpio_width[:-1] + "'b" + io_value[:-1] + ";})")
+			output.write("\nendtask;")
+			output.write("\n\n`endif")                   
 					
 
-
-
-extract(filename,"prog_cycle_task(","		@(negedge prog_clock[0]);")
+extract_parameters(sim_file)
