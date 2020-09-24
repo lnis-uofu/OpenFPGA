8e780635df5781eb5a92cafcfa9b5ca59d53adb8
tangxifan <tangxifan@gmail.com>
2020-09-23 22:59:46 -0600

    [Regression Test] Rename test case in CI

--- .travis/basic_reg_test.sh
+++ .travis/basic_reg_test.sh
@@ -23,7 +23,7 @@ python3 openfpga_flow/scripts/run_fpga_task.py basic_tests/full_testbench/fast_c
 python3 openfpga_flow/scripts/run_fpga_task.py basic_tests/preconfig_testbench/configuration_frame --debug --show_thread_logs
 python3 openfpga_flow/scripts/run_fpga_task.py basic_tests/full_testbench/configuration_frame_ccff --debug --show_thread_logs
 python3 openfpga_flow/scripts/run_fpga_task.py basic_tests/full_testbench/configuration_frame_scff --debug --show_thread_logs
-python3 openfpga_flow/scripts/run_fpga_task.py basic_tests/full_testbench/configuration_frame_resetb --debug --show_thread_logs
+python3 openfpga_flow/scripts/run_fpga_task.py basic_tests/full_testbench/configuration_frame_use_resetb --debug --show_thread_logs
 
 echo -e "Testing memory bank configuration protocol of a K4N4 FPGA";
 python3 openfpga_flow/scripts/run_fpga_task.py basic_tests/full_testbench/memory_bank --debug --show_thread_logs
