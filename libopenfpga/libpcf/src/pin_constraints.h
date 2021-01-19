52ac7826ebfef2d0efb541719994275734382989
tangxifan <tangxifan@gmail.com>
2021-01-19 15:45:45 -0700

    [Lib] Add a library of parser/writer for pin constraint file (PCF)

---
+++ libopenfpga/libpcf/src/pin_constraints.h
@@ -0,0 +1,77 @@
+#ifndef PIN_CONSTRAINTS_H
+#define PIN_CONSTRAINTS_H
+
+/********************************************************************
+ * This file include the declaration of pin constraints
+ *******************************************************************/
+#include <string>
+#include <map>
+#include <array>
+
+/* Headers from vtrutil library */
+#include "vtr_vector.h"
+#include "vtr_geometry.h"
+
+/* Headers from openfpgautil library */
+#include "openfpga_port.h"
+
+#include "pin_constraints_fwd.h"
+
+/********************************************************************
+ * A data structure to describe the pin constraints for FPGA fabrics
+ * This data structure may include a number of pin constraints
+ * each of which may constrain:
+ * - pin assignment, for instance, force a net to be mapped to specific pin
+ *
+ * Typical usage:
+ * --------------
+ *   // Create an object of pin constraints
+ *   PinConstraints pin_constraints;
+ *   // Add a pin assignment
+ *   openfpga::BasicPort pin_info(clk, 1);
+ *   std::string net_info("top_clock");
+ *   PinConstraintId pin_constraint_id = pin_constraints.create_pin_constraint(pin_info, net_info);
+ *
+ *******************************************************************/
+class PinConstraints {
+  public: /* Types */
+    typedef vtr::vector<PinConstraintId, PinConstraintId>::const_iterator pin_constraint_iterator;
+    /* Create range */
+    typedef vtr::Range<pin_constraint_iterator> pin_constraint_range;
+  public:  /* Constructors */
+    PinConstraints();
+  public: /* Accessors: aggregates */
+    pin_constraint_range pin_constraints() const;
+  public: /* Public Accessors: Basic data query */
+    /* Get the pin to be constrained */
+    openfpga::BasicPort pin(const PinConstraintId& pin_constraint_id) const;
+
+    /* Get the net to be constrained */
+    std::string net(const PinConstraintId& pin_constraint_id) const;
+
+    /* Check if there are any pin constraints */
+    bool empty() const;
+
+  public: /* Public Mutators */
+
+    /* Reserve a number of design constraints to be memory efficent */
+    void reserve_pin_constraints(const size_t& num_pin_constraints);
+
+    /* Add a pin constraint to storage */
+    PinConstraintId create_pin_constraint(const openfpga::BasicPort& pin,
+                                          const std::string& net);
+
+  public: /* Public invalidators/validators */
+    bool valid_pin_constraint_id(const PinConstraintId& pin_constraint_id) const;
+  private: /* Internal data */
+    /* Unique ids for each design constraint */
+    vtr::vector<PinConstraintId, PinConstraintId> pin_constraint_ids_;
+
+    /* Pins to constraint */
+    vtr::vector<PinConstraintId, openfpga::BasicPort> pin_constraint_pins_;
+
+    /* Nets to constraint */
+    vtr::vector<PinConstraintId, std::string> pin_constraint_nets_;
+};
+
+#endif
---
+++ libopenfpga/libpcf/src/write_xml_pin_constraints.cpp
@@ -0,0 +1,94 @@
+/********************************************************************
+ * This file includes functions that outputs a pin constraint object to XML format
+ *******************************************************************/
+/* Headers from system goes first */
+#include <string>
+#include <algorithm>
+
+/* Headers from vtr util library */
+#include "vtr_assert.h"
+#include "vtr_log.h"
+#include "vtr_time.h"
+
+/* Headers from openfpga util library */
+#include "openfpga_digest.h"
+
+/* Headers from arch openfpga library */
+#include "write_xml_utils.h" 
+
+/* Headers from pin constraint library */
+#include "write_xml_pin_constraints.h"
+
+/********************************************************************
+ * A writer to output a pin constraint to XML format
+ *
+ * Return 0 if successful
+ * Return 1 if there are more serious bugs in the architecture 
+ * Return 2 if fail when creating files
+ *******************************************************************/
+static 
+int write_xml_io_constraint(std::fstream& fp,
+                            const PinConstraints& pin_constraints,
+                            const PinConstraintId& pin_constraint) {
+  /* Validate the file stream */
+  if (false == openfpga::valid_file_stream(fp)) {
+    return 2;
+  }
+
+  openfpga::write_tab_to_file(fp, 1);
+  fp << "<set_io";
+
+  if (false == pin_constraints.valid_pin_constraint_id(pin_constraint)) {
+    return 1;
+  }
+
+  write_xml_attribute(fp, "pin", generate_xml_port_name(pin_constraints.pin(pin_constraint)).c_str());
+  write_xml_attribute(fp, "net", pin_constraints.net(pin_constraint).c_str());
+
+  fp << "/>" << "\n";
+
+  return 0;
+}
+
+/********************************************************************
+ * A writer to output a repack pin constraint object to XML format
+ *
+ * Return 0 if successful
+ * Return 1 if there are more serious bugs in the architecture 
+ * Return 2 if fail when creating files
+ *******************************************************************/
+int write_xml_pin_constraints(const char* fname,
+                              const PinConstraints& pin_constraints) {
+
+  vtr::ScopedStartFinishTimer timer("Write Pin Constraints");
+
+  /* Create a file handler */
+  std::fstream fp;
+  /* Open the file stream */
+  fp.open(std::string(fname), std::fstream::out | std::fstream::trunc);
+
+  /* Validate the file stream */
+  openfpga::check_file_stream(fname, fp);
+  
+  /* Write the root node */
+  fp << "<pin_constraints>" << "\n";
+
+  int err_code = 0;
+
+  /* Write region by region */ 
+  for (const PinConstraintId& pin_constraint : pin_constraints.pin_constraints()) {
+    /* Write constraint by constraint */ 
+    err_code = write_xml_pin_constraint(fp, pin_constraints, pin_constraint);
+    if (0 != err_code) {
+      return err_code;
+    }
+  }
+
+  /* Finish writing the root node */
+  fp << "</pin_constraints>" << "\n";
+
+  /* Close the file stream */
+  fp.close();
+
+  return err_code;
+}
