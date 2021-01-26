ec6821fafd7776d98a0bb8feacf8ca0662c1792e
Ganesh Gore <goreganesh007@gmail.com>
2021-01-26 15:08:25 -0700

    [CI/CD] Moved pip installation to dockerfile

--- .github/workflows/install_dependencies_run.sh
+++ .github/workflows/install_dependencies_run.sh
@@ -2,7 +2,3 @@ apt-get install --no-install-recommends -y \
 libdatetime-perl libc6 libffi6 libgcc1 libreadline7 libstdc++6 \
 libtcl8.6 python3.8 python3-pip zlib1g libbz2-1.0 \
 iverilog git rsync make curl
-
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.8 get-pip.py
-rm get-pip.py
