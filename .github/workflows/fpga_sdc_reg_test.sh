2d4c89d23c18e7006db40d65885c0451812fc91e
Ganesh Gore <goreganesh007@gmail.com>
2021-01-26 15:31:33 -0700

    [CI/CD] Running regression test with run-task

--- .github/workflows/fpga_sdc_reg_test.sh
+++ .github/workflows/fpga_sdc_reg_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-
+source openfpga.sh
 ###############################################
 # OpenFPGA Shell with VPR8
 ##############################################
 echo -e "FPGA-SDC regression tests";
 
 echo -e "Testing SDC generation with time units";
-python3 openfpga_flow/scripts/run_fpga_task.py fpga_sdc/sdc_time_unit --debug --show_thread_logs
+run-task fpga_sdc/sdc_time_unit --debug --show_thread_logs
--- .github/workflows/fpga_spice_reg_test.sh
+++ .github/workflows/fpga_spice_reg_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-
+source openfpga.sh
 ###############################################
 # OpenFPGA Shell with VPR8
 ##############################################
 echo -e "FPGA-SPICE regression tests";
 
 echo -e "Testing FPGA-SPICE with netlist generation";
-python3 openfpga_flow/scripts/run_fpga_task.py fpga_spice/generate_spice --debug --show_thread_logs
+run-task fpga_spice/generate_spice --debug --show_thread_logs
