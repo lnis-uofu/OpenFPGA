d5b1cc5ec7c94acc412398f5fd26703b75ee3de1
tangxifan <tangxifan@gmail.com>
2021-02-01 18:01:42 -0700

    [Tool] Bug fix in parser for bitstream settings

--- libopenfpga/libarchopenfpga/test/read_bitstream_setting_openfpga.cpp
+++ libopenfpga/libarchopenfpga/test/read_bitstream_setting_openfpga.cpp
@@ -16,7 +16,7 @@ int main(int argc, const char** argv) {
   VTR_ASSERT((2 == argc) || (3 == argc));
 
   /* Parse the simulation settings from an XML file */
-  const openfpga::BitstreamSetting& openfpga_sim_setting = read_xml_openfpga_bitstream_settings(argv[1]); 
+  const openfpga::BitstreamSetting& openfpga_bitstream_setting = read_xml_openfpga_bitstream_settings(argv[1]); 
   VTR_LOG("Parsed bitstream settings from XML %s.\n",
           argv[1]);
 
