0c6d27cf7ea056db2c82fa0b1578dd7faf9df469
Nachiket Kapre <nachiket@uwaterloo.ca>
2021-02-08 21:26:48 -0500

    merge for consideration;

---
+++ openfpga_flow/openfpga_cell_library/verilog/dffsrq.v
@@ -0,0 +1,36 @@
+module DFFSRQ(SET,
+            RST,
+            CK,
+            D,
+            Q);
+//----- GLOBAL PORTS -----
+input [0:0] SET;
+//----- GLOBAL PORTS -----
+input [0:0] RST;
+//----- GLOBAL PORTS -----
+input [0:0] CK;
+//----- INPUT PORTS -----
+input [0:0] D;
+//----- OUTPUT PORTS -----
+output reg [0:0] Q;
+
+//----- BEGIN wire-connection ports -----
+//----- END wire-connection ports -----
+
+
+//----- BEGIN Registered ports -----
+//----- END Registered ports -----
+
+// ----- Internal logic should start here -----
+always @(posedge CK) begin
+  if(RST) begin
+    Q <= 1'b0;
+  else if(SET) begin
+    Q <= 1'b1;
+  end else begin
+    Q <= D;
+  end
+end
+
+// ----- Internal logic should end here -----
+endmodule
---
+++ openfpga_flow/openfpga_cell_library/verilog/inv.v
@@ -0,0 +1,29 @@
+// ----- Verilog module for INVTX1 -----
+module INVTX1(in,
+              out);
+//----- INPUT PORTS -----
+input [0:0] in;
+//----- OUTPUT PORTS -----
+output [0:0] out;
+
+//----- BEGIN wire-connection ports -----
+//----- END wire-connection ports -----
+
+
+//----- BEGIN Registered ports -----
+//----- END Registered ports -----
+
+// ----- Verilog codes of a regular inverter -----
+  //assign out = (in === 1'bz)? $random : ~in;
+  assign out = ~in;
+
+`ifdef ENABLE_TIMING
+// ------ BEGIN Pin-to-pin Timing constraints -----
+  specify
+    (in[0] => out[0]) = (0.01, 0.01);
+  endspecify
+// ------ END Pin-to-pin Timing constraints -----
+`endif
+endmodule
+// ----- END Verilog module for INVTX1 -----
+
