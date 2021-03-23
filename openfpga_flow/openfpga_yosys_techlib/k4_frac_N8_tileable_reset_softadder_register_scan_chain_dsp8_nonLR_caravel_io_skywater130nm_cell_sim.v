a4bbffd1aa8a7dc019a3f4bbb9fa0bdae80de1d6
tangxifan <tangxifan@gmail.com>
2021-03-23 15:30:41 -0600

    [HDL] Add yosys tech lib for a DSP-only heterogeneous FPGA

---
+++ openfpga_flow/openfpga_yosys_techlib/k4_frac_N8_tileable_reset_softadder_register_scan_chain_dsp8_nonLR_caravel_io_skywater130nm_cell_sim.v
@@ -0,0 +1,12 @@
+//-----------------------------
+// 8-bit multiplier
+//-----------------------------
+module mult_8(
+  input [0:7] A,
+  input [0:7] B,
+  output [0:15] Y
+);
+
+assign Y = A * B;
+
+endmodule
