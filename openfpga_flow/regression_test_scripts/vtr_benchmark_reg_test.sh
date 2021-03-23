fff16a01ab6314e831e86c82e72c27ba41595372
tangxifan <tangxifan@gmail.com>
2021-03-23 12:27:20 -0600

    [Test] Update tolerance when checking VTR benchmark QoR

--- openfpga_flow/regression_test_scripts/vtr_benchmark_reg_test.sh
+++ openfpga_flow/regression_test_scripts/vtr_benchmark_reg_test.sh
@@ -8,5 +8,5 @@ PYTHON_EXEC=python3.8
 ##############################################
 echo -e "VTR benchmark regression tests";
 run-task benchmark_sweep/vtr_benchmarks --debug --show_thread_logs
-# Run a quick QoR check for heterogeneous blocks
-python3 openfpga_flow/scripts/check_qor.py --reference_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/config/vtr_benchmark_golden_results.csv --check_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/latest/task_result.csv --metric_checklist_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/config/metric_checklist.csv
+# Run a quick but relaxed QoR check for heterogeneous blocks
+python3 openfpga_flow/scripts/check_qor.py --reference_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/config/vtr_benchmark_golden_results.csv --check_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/latest/task_result.csv --metric_checklist_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/config/metric_checklist.csv --check_tolerance 0.2,100
