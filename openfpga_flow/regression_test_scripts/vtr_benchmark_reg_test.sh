e3f8a6cf7a45b9269302f197e760472c89422bea
tangxifan <tangxifan@gmail.com>
2021-03-23 11:15:22 -0600

    [Test] Deploy QoR check to VTR benchmark regression test

--- openfpga_flow/regression_test_scripts/vtr_benchmark_reg_test.sh
+++ openfpga_flow/regression_test_scripts/vtr_benchmark_reg_test.sh
@@ -8,3 +8,5 @@ PYTHON_EXEC=python3.8
 ##############################################
 echo -e "VTR benchmark regression tests";
 run-task benchmark_sweep/vtr_benchmarks --debug --show_thread_logs
+# Run a quick QoR check for heterogeneous blocks
+python3 openfpga_flow/scripts/check_qor.py --reference_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/config/vtr_benchmark_golden_results.csv --check_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/latest/task_result.csv --metric_checklist_csv_file openfpga_flow/tasks/benchmark_sweep/vtr_benchmarks/config/metric_checklist.csv
