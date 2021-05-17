9adde5b3bab375b8edef6048c2771269db11e3a9
BhavaniSampath <79945238+BhavaniSampath@users.noreply.github.com>
2021-05-17 14:22:29 -0600

    Update AES_main.cpp

--- libopenfpga/libfpgaaesencryption/test/AES_main.cpp
+++ libopenfpga/libfpgaaesencryption/test/AES_main.cpp
@@ -1,10 +1,49 @@
 #include "AESfunctions.h"
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include<cctype>
+
+//String conversion function
+string convertToString(char* a, int size)
+{
+    int i;
+    string s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
+}
+
+//Function to convert binary to Decimal
+
+int binaryToDecimal(int n)
+{
+    int num = n;
+ int dec_value = 0;
+
+    // Initializing base value to 1, i.e 2^0
+     int base = 1;
+
+    int temp = num;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+
+        dec_value += last_digit * base;
+
+        base = base * 2;
+    }
+
+    return dec_value;
+}
 
 int main()
 {
     // These variables allow to change the AES length
-	// Nk = 4, 6 or 8 for AES 128, 192 or 256 respectively
-	// Only set Nk and the rest will compute according to the AES specification
+    // Nk = 4, 6 or 8 for AES 128, 192 or 256 respectively
+    // Only set Nk and the rest will compute according to the AES specification
     unsigned short Nk = 4; // 4 or 6 or 8 [32-bit words] columns in cipher key
     unsigned short CipherKeyLenghth = Nk * rows; // bytes
     unsigned short Nr = (Nk > Nb) ? Nk + 6 : Nb + 6; // = 10, 12 or 14 rounds
@@ -33,33 +72,122 @@ int main()
     cout << endl << endl;
 
 
-    // create a test input data (plaintext) (ABCDEFGHIJKLMNOP)
-    unsigned char plaintext[stt_lng] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' };
-    cout << "plaintext = ";
-    for (unsigned short i = 0; i < stt_lng; i++) { cout << plaintext[i] << " "; }
-    cout << "<=> ";
-    for (unsigned short i = 0; i < stt_lng; i++) { cout << "0x" << hex << (unsigned short)plaintext[i] << " "; }
-    cout << endl << endl;
+// create a test input data (plaintext) (ABCDEFGHIJKLMNOP)
 
+streampos size;
+char *data;
+unsigned int plaintext[16];
+int j=0;
+int k;
+int newsize;
+int y;
+ifstream file ("fabric_bitstream.txt",ios::in|ios::ate);
+if (file.is_open())
+{
+size=file.tellg();
+//cout<<size<<endl;
+data=new char [size];
+file.seekg(0,ios::beg);
+file.read(data,size);
+file.close();
+//cout<<data<<endl;
+}
+else cout<<"Unable to open file";
+//cout<<size<<endl;
+std::string s,new_s;
+std::stringstream ss;
+ss << data;
+//ss >> s;
+s = ss.str();
+char new_data[4];
+int count=0,i=0;
+newsize=(size/8) +1;
+int z[newsize];
+int w[newsize];
+//for(int i=0;i<size;i++)
+while(i<size)
+{
+        while(count<4)
+        {
+//      cout<<i<<"="<<s[i]<<endl;
+ if(s[i]=='1')
+        {
+         new_data[count]=s[i];
+        count=count+1;
+ i++;
+        }
+        if(s[i]=='0'){
 
-    // encrypt
-    unsigned char ciphertext[stt_lng];
+        new_data[count]=s[i];
+        count=count+1;
+        i++;
+}
+        else
+        i++;
+}
+new_s = convertToString(new_data,4);
+//  cout<<new_data<<endl;
+stringstream ssn(new_s);
+int x=0;
+ssn>>x;
+
+ count=0;
+y=binaryToDecimal(x);
+//cout<<y<<endl;
+z[j]=y;
+//cout<<z[j]<<endl;
+j++;
+
+}
+//cout<<size<<endl;
+int arr_size=((sizeof z) / (sizeof *z));
+//cout<<arr_size;
+int l=0,n=0,counter=0;
+k=0;
+while(n<arr_size)
+{
+for (l=0;l<16;l++){
+if(n<arr_size){
+ plaintext[l]=z[n];
+n++;
+}
+else
+ plaintext[l]=0;
+//cout<<plaintext[l];
+}
+//cout<<endl;
+cout<<"round"<<dec<<counter<<":"<<"plaintext:";
+for(l=0;l<16;l++)
+{
+cout<<dec<<plaintext[l]<<"";
+}
+cout<<"<=>";
+for (int k=0;k<16;k++)
+{
+cout<<"0X"<<hex<<plaintext[k]<<"";
+}
+cout<<endl;
+
+// encrypt
+     unsigned char ciphertext[stt_lng];
     AES_Encrypt(plaintext, expandedKey, Nr, ciphertext);
     cout << "ciphertext = ";
-    for (unsigned short i = 0; i < stt_lng; i++) { cout << dec << (unsigned short)ciphertext[i] << " "; }
+    for (unsigned short i = 0; i <stt_lng; i++) { cout << dec << (unsigned short)ciphertext[i] << " "; }
     cout << "<=> ";
     for (unsigned short i = 0; i < stt_lng; i++) { cout << "0x" << hex << (unsigned short)ciphertext[i] << " "; }
     cout << endl << endl;
-
+counter++;
+}
 /*
-    // decrypt
-    unsigned char decrypted_plaintext[stt_lng];
-    AES_Decrypt(ciphertext, expandedKey, Nr, decrypted_plaintext);
-    cout << "decrypted_plaintext = ";
-    for (unsigned short i = 0; i < stt_lng; i++) { cout << decrypted_plaintext[i] << " "; }
-    cout << "<=> ";
-    for (unsigned short i = 0; i < stt_lng; i++) { cout << "0x" << hex << (unsigned short)decrypted_plaintext[i] << " "; }
-    cout << endl << endl;
-*/
+ *
+ *     // decrypt
+ *     unsigned char decrypted_plaintext[stt_lng];
+ *         AES_Decrypt(ciphertext, expandedKey, Nr, decrypted_plaintext);
+ *             cout << "decrypted_plaintext = ";
+ *                 for (unsigned short i = 0; i < stt_lng; i++) { cout << decrypted_plaintext[i] << " "; }
+ *                     cout << "<=> ";
+ *                         for (unsigned short i = 0; i < stt_lng; i++) { cout << "0x" << hex << (unsigned short)decrypted_plaintext[i] << " "; }
+ *                          cout << endl << endl;
+ *                          */
     return 0;
 }
