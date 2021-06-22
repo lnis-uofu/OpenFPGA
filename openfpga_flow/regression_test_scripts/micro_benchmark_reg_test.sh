e34fbf8ecf8f7769fd650ab48e2200997a1a60e5
tangxifan <tangxifan@gmail.com>
2021-06-22 16:36:04 -0600

    [Test] Deploy MCNC big20 to the micro benchmark regression test

--- openfpga_flow/regression_test_scripts/micro_benchmark_reg_test.sh
+++ openfpga_flow/regression_test_scripts/micro_benchmark_reg_test.sh
@@ -7,6 +7,11 @@ PYTHON_EXEC=python3.8
 # OpenFPGA Shell with VPR8
 ##############################################
 echo -e "Micro benchmark regression tests";
-# TODO: enable this when flow-run script support same top module name in one task configuration file
-#run-task benchmark_sweep/counter --debug --show_thread_logs
+run-task benchmark_sweep/counter --debug --show_thread_logs
 run-task benchmark_sweep/mac_units --debug --show_thread_logs
+
+# Verify MCNC big20 benchmark suite with ModelSim
+# Please make sure you have ModelSim installed in the environment
+# Otherwise, it will fail
+run-task benchmark_sweep/mcnc_big20 --debug --show_thread_logs
+#python3 openfpga_flow/scripts/run_modelsim.py mcnc_big20 --run_sim
