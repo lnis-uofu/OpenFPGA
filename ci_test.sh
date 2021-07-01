6559f7108227ff3a68a91e06409b9842950c7c01
komaljaved-rs <komal.javed@rapidsilicon.com>
2021-07-01 15:07:37 +0500

    added ci_scripts

---
+++ ci_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export OPENFPGA_PATH=/home/komal.javed@lmlhr.com/rapidsilicon/openfpga_new
+
+for dir in RTL_Benchmark/*; do
+ if [ -d "$dir" ]; then
+	echo "Looking for top file in $dir/rtl"
+
+	top_file="$(basename "$dir")"
+
+	FILE=$dir/rtl/$top_file.v
+
+	if [ -f "$FILE" ]; then
+    	  echo -e "${CYAN}$top_file exists ${ENDCOLOR}"
+	  export design_path=${OPENFPGA_PATH}/$FILE
+	  export design_top=$top_file
+	  echo "design = $design_path"
+	  python3 openfpga_flow/scripts/run_ci_tests.py ci_tests
+	else  
+	  echo -e "${RED}Top file not found. Make sure design exists${ENDCOLOR}"
+	  exit 1
+	fi
+ fi
+done
+
+
