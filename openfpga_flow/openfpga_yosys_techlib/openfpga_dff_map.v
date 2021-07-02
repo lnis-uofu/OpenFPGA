02fd2a69b313fb7c84a248e4503e4a0b64459b07
tangxifan <tangxifan@gmail.com>
2021-07-02 11:17:43 -0600

    [Script] Add dff with active-low async reset to default yosys tech lib

--- openfpga_flow/openfpga_yosys_techlib/openfpga_dff_map.v
+++ openfpga_flow/openfpga_yosys_techlib/openfpga_dff_map.v
@@ -17,6 +17,16 @@ module \$_DFF_PP0_ (D, C, R, Q);
     dffr _TECHMAP_REPLACE_ (.Q(Q), .D(D), .C(C), .R(R));
 endmodule
 
+// Async active-low reset
+module \$_DFF_PN0_ (D, C, R, Q);
+    input D;
+    input C;
+    input R;
+    output Q;
+    parameter _TECHMAP_WIREINIT_Q_ = 1'bx;
+    dffrn _TECHMAP_REPLACE_ (.Q(Q), .D(D), .C(C), .RN(R));
+endmodule
+
 // Async reset, enable
 module  \$_DFFE_PP0P_ (D, C, E, R, Q);
     input D;
