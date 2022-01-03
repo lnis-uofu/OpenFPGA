628191da5f24ba890a6301b0c3874663c1c458e6
tangxifan <tangxifan@gmail.com>
2022-01-02 20:21:58 -0800

    [Test] Add new test case (DSP with registers) into FPGA-Verilog regression tests

--- openfpga_flow/regression_test_scripts/fpga_verilog_reg_test.sh
+++ openfpga_flow/regression_test_scripts/fpga_verilog_reg_test.sh
@@ -53,6 +53,9 @@ run-task fpga_verilog/dsp/multi_mode_mult_16x16 --debug --show_thread_logs
 echo -e "Testing Verilog generation with heterogeneous fabric using multi-width 16-bit multi-mode multipliers ";
 run-task fpga_verilog/dsp/wide_multi_mode_mult_16x16 --debug --show_thread_logs
 
+echo -e "Testing Verilog generation with heterogeneous fabric using 8-bit single-mode registerable multipliers ";
+run-task fpga_verilog/dsp/single_mode_mult_8x8_reg --debug --show_thread_logs
+
 echo -e "Testing Verilog generation with different I/O capacities on each side of an FPGA ";
 run-task fpga_verilog/io/multi_io_capacity --debug --show_thread_logs
 
