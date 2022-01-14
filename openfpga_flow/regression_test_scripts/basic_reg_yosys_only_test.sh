469b3a960cc38826a0e5975419ae28412690ffa8
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-01-14 15:44:26 +0500

    basic reg test updated

--- openfpga_flow/regression_test_scripts/basic_reg_yosys_only_test.sh
+++ openfpga_flow/regression_test_scripts/basic_reg_yosys_only_test.sh
@@ -9,8 +9,8 @@ PYTHON_EXEC=python3.8
 echo -e "Basic regression tests for Yosys-only flow";
 
 echo -e "Testing configuration chain of a K4N4 FPGA";
-run-task basic_tests/verific_test --debug --show_thread_logs
+run-task basic_tests/yosys_only --debug --show_thread_logs
 
 # Repgression test to test multi-user enviroment
-cp -r */*/basic_tests/full_testbench/configuration_chain /tmp/
-cd /tmp/ && run-task configuration_chain --debug --show_thread_logs
+cp -r */*/basic_tests/yosys_only /tmp/
+cd /tmp/ && run-task yosys_only --debug --show_thread_logs
