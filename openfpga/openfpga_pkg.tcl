27745dd609838fbf262368e0e43f0ccd24eb14cd
nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
2022-03-08 17:59:32 +0500

    TCK pkg now have all the function named as command

--- openfpga/openfpga_pkg.tcl
+++ openfpga/openfpga_pkg.tcl
@@ -3,10 +3,37 @@
 set home [file join [pwd] [file dirname [info script]]]
 #Load the shared library
 catch { load $home/libopenfpga[info sharedlibextension] openfpga}
-# Created OpenFPGA API cpp class object with name OPENFPGA"
-openfpga_api OPENFPGA
-puts "OpenFPGA shell is initialised"
-puts "Use OpenFPGA command like OPENFPGA read_arch -f <path to architecture file>"
+# OpenFPGA class have all the command name functions
+oo::class create OpenFPGA_API
+oo::define OpenFPGA_API {
+    method ComndHelp { args } {
+       puts $args
+    }
+
+    method read_arch { args } {
+         if { ( $args eq "" ) || ( $args eq {-help} ) || ( $args eq {-h} ) } {
+            return [ my ComndHelp "\nCommand OPENFPGA read_arch usage:\n\n OPENFPGA read_arch -f or --file <path to architecture XML>\n"]
+         }
+         set comnd read_openfpga_arch
+         foreach item $args {
+            lappend comnd $item
+         }
+         #puts "the given arguments are\n$comnd"
+         openfpga_api "$comnd"
+    }
+}
+
+oo::define OpenFPGA_API {
+    constructor {} {
+        #puts "called constructor"
+        openfpga_api "call_title"
+    }
+    destructor {  1
+        puts "[self] saving OpenFPGA_API data to database"
+    }
+}
+
+OpenFPGA_API create OPENFPGA
 # Variables for version and testing
 set version 1.0
 set testvar "Hello from OpenFPGA"
--- openfpga/src/invoker/openfpga_api.h
+++
@@ -1,9 +0,0 @@
-class openfpga_api {
- public:
-  openfpga_api();
-  ~openfpga_api();
-  void version();
-  void start_tcl_shell();
-  void read_arch (std::string opt, std::string path);
-};
-
