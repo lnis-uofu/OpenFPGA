7b71abf7a0a7ca2de07ad18ed8f730f8b158e0c1
nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
2022-03-21 19:04:37 +0500

    added a function to enable all the supported commands by OpenFPGA mini shell

--- openfpga/openfpga_pkg.tcl
+++ openfpga/openfpga_pkg.tcl
@@ -5,54 +5,16 @@ set home [file join [pwd] [file dirname [info script]]]
 catch { load $home/libopenfpga[info sharedlibextension] openfpga}
 # create OpenFPGA API cpp classe object
 openfpga_api tool
-# OpenFPGA class have all the command name functions
-oo::class create OpenFPGA_TCL_API
-oo::define OpenFPGA_TCL_API {
-    method ComndHelp { args } {
-       puts $args
-    }
-
-    method version { } {
-         #puts "the given arguments are\n$comnd"
-         tool version
-    }
-
-    method read_arch { args } {
-         if { ( $args eq "" ) || ( $args eq {-help} ) || ( $args eq {-h} ) } {
-            return [ my ComndHelp "\nCommand OPENFPGA read_arch usage:\n\n OPENFPGA read_arch -f or --file <path to architecture XML>\n"]
-         }
-         set comnd read_openfpga_arch
-         foreach item $args {
-            lappend comnd $item
-         }
-         #puts "the given arguments are\n$comnd"
-         tool call_openfpga_shell "$comnd"
-    }
-     method write_arch { args } {
-         if { ( $args eq "" ) || ( $args eq {-help} ) || ( $args eq {-h} ) } {
-            return [ my ComndHelp "\nCommand OPENFPGA write_arch usage:\n\n OPENFPGA write_arch -f or --file <specify the architecture XML name>\n"]
-         }
-         set comnd write_openfpga_arch
-         foreach item $args {
-            lappend comnd $item
-         }
-         #puts "the given arguments are\n$comnd"
-         tool call_openfpga_shell "$comnd"
-    }
+# function to be called with command
+proc OPENFPGA { args } {
+   if { ( $args eq "" ) } {
+            puts "Error: supply at least one command like OPENFPGA version"
+         } else {
+   tool call_openfpga_shell "$args"
+   }
 }
-
-oo::define OpenFPGA_TCL_API {
-    constructor {} {
-        #puts "called constructor"
-        tool call_openfpga_shell "call_title"
-    }
-    destructor {  1
-        puts "[self] saving OpenFPGA_TCL_API data to database"
-    }
-}
-
-# TODO count overall time of execution in TCL
-OpenFPGA_TCL_API create OPENFPGA
+# print title
+OPENFPGA "call_title"
 # Variables for version and testing
 set version 1.0
 set testvar "Hello from OpenFPGA"
@@ -68,4 +30,4 @@ if {$::tcl_interactive} {
    }
 
 package provide OpenFPGA $version
-package require Tcl 8.0
+package require Tcl 8.6
