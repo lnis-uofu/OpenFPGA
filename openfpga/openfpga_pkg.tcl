43fc2261091053a98edad610176cf037adbd7e29
nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
2022-03-24 19:35:54 +0500

    now tcl script has only class instance

--- openfpga/openfpga_pkg.tcl
+++ openfpga/openfpga_pkg.tcl
@@ -4,18 +4,8 @@ set home [file join [pwd] [file dirname [info script]]]
 #Load the shared library of OpenFPGA
 catch { load $home/libopenfpga[info sharedlibextension] openfpga}
 # create OpenFPGA API cpp classe object
-openfpga_api tool
-# function to be called with command
-proc OPENFPGA { args } {
-   if { ( $args eq "" ) } {
-            puts "Error: supply at least one command like OPENFPGA version"
-         } else {
-   tool call_openfpga_shell "$args"
-   }
-}
-# print title
-OPENFPGA "call_title"
-# Variables for version and testing
+openfpga_api OPENFPGA
+
 set version 1.0
 set testvar "Hello from OpenFPGA"
 # Test procedure
