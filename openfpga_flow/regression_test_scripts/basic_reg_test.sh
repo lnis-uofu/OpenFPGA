7d694acf32d8a63a412c005a2bd27c0a46fc51bf
tangxifan <tangxifan@gmail.com>
2022-05-23 11:21:36 +0800

    [test] debugging basic reg test paths

--- openfpga_flow/regression_test_scripts/basic_reg_test.sh
+++ openfpga_flow/regression_test_scripts/basic_reg_test.sh
@@ -181,6 +181,9 @@ run-task basic_tests/no_time_stamp $@
 # Run git-diff to ensure no changes on the golden netlists
 # Switch to root path in case users are running the tests in another location
 cd ${OPENFPGA_PATH}
+pwd
+git config --global --add safe.directory ${OPENFPGA_PATH}
+git log
 git diff --name-status -- ':${OPENFPGA_PATH}/openfpga_flow/tasks/basic_tests/no_time_stamp/golden_outputs_no_time_stamp/**'
 if git diff --name-status --exit-code -- ':${OPENFPGA_PATH}/openfpga_flow/tasks/basic_tests/no_time_stamp/golden_outputs_no_time_stamp/**'; then
   echo -e "Golden netlist remain unchanged"
