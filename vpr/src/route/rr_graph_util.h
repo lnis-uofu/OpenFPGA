075900a7c9a8e87390dc60e4cbc0faa3287a535b
tangxifan <tangxifan@gmail.com>
2022-08-16 13:56:08 -0700

    [engine] remove out-of-date codes due to the upgrades in VTR submodule

--- vpr/src/route/rr_graph_util.h
+++
@@ -1,8 +0,0 @@
-#ifndef RR_GRAPH_UTIL_H
-#define RR_GRAPH_UTIL_H
-
-int seg_index_of_cblock(t_rr_type from_rr_type, const RRNodeId& to_node);
-
-int seg_index_of_sblock(const RRNodeId& from_node, const RRNodeId& to_node);
-
-#endif
