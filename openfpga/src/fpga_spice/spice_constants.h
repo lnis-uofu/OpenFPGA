dfe30df4626aacd07fd8b2f34dffca437b1e7aac
tangxifan <tangxifan@gmail.com>
2022-08-17 16:32:21 -0700

    [engine] resolve compilation warnings

--- openfpga/src/fpga_spice/spice_constants.h
+++ openfpga/src/fpga_spice/spice_constants.h
@@ -4,26 +4,26 @@
 /* global parameters for dumping spice netlists */
 constexpr size_t SPICE_NETLIST_MAX_NUM_PORTS_PER_LINE = 10;
 
-constexpr char* SPICE_NETLIST_FILE_POSTFIX = ".sp";
+constexpr const char* SPICE_NETLIST_FILE_POSTFIX = ".sp";
 
-constexpr char* TRANSISTOR_WRAPPER_POSTFIX = "_wrapper";
+constexpr const char* TRANSISTOR_WRAPPER_POSTFIX = "_wrapper";
 
-constexpr char* TRANSISTORS_SPICE_FILE_NAME = "transistor.sp";
-constexpr char* SUPPLY_WRAPPER_SPICE_FILE_NAME = "supply_wrapper.sp";
-constexpr char* MUX_PRIMITIVES_SPICE_FILE_NAME = "mux_primitives.sp";
-constexpr char* MUXES_SPICE_FILE_NAME = "muxes.sp";
-constexpr char* LUTS_SPICE_FILE_NAME = "luts.sp";
-constexpr char* MEMORIES_SPICE_FILE_NAME = "memories.sp";
-constexpr char* FABRIC_INCLUDE_SPICE_NETLIST_FILE_NAME = "fabric_netlists.sp";
+constexpr const char* TRANSISTORS_SPICE_FILE_NAME = "transistor.sp";
+constexpr const char* SUPPLY_WRAPPER_SPICE_FILE_NAME = "supply_wrapper.sp";
+constexpr const char* MUX_PRIMITIVES_SPICE_FILE_NAME = "mux_primitives.sp";
+constexpr const char* MUXES_SPICE_FILE_NAME = "muxes.sp";
+constexpr const char* LUTS_SPICE_FILE_NAME = "luts.sp";
+constexpr const char* MEMORIES_SPICE_FILE_NAME = "memories.sp";
+constexpr const char* FABRIC_INCLUDE_SPICE_NETLIST_FILE_NAME = "fabric_netlists.sp";
 
-constexpr char* SPICE_SUBCKT_VDD_PORT_NAME = "VDD";
-constexpr char* SPICE_SUBCKT_GND_PORT_NAME = "VSS";
+constexpr const char* SPICE_SUBCKT_VDD_PORT_NAME = "VDD";
+constexpr const char* SPICE_SUBCKT_GND_PORT_NAME = "VSS";
 
-constexpr char* SPICE_MUX_BASIS_POSTFIX = "_basis";
-constexpr char* SPICE_MEM_POSTFIX = "_mem";
+constexpr const char* SPICE_MUX_BASIS_POSTFIX = "_basis";
+constexpr const char* SPICE_MEM_POSTFIX = "_mem";
 
-constexpr char* SB_SPICE_FILE_NAME_PREFIX = "sb_";
-constexpr char* LOGICAL_MODULE_SPICE_FILE_NAME_PREFIX = "logical_tile_";
-constexpr char* GRID_SPICE_FILE_NAME_PREFIX = "grid_";
+constexpr const char* SB_SPICE_FILE_NAME_PREFIX = "sb_";
+constexpr const char* LOGICAL_MODULE_SPICE_FILE_NAME_PREFIX = "logical_tile_";
+constexpr const char* GRID_SPICE_FILE_NAME_PREFIX = "grid_";
 
 #endif
