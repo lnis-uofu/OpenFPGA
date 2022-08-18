2d05462219d811a7368edf0bc820d562c8366654
tangxifan <tangxifan@gmail.com>
2022-08-18 15:53:51 -0700

    [lib] remove warnings

--- libs/libbusgroup/src/bus_group_xml_constants.h
+++ libs/libbusgroup/src/bus_group_xml_constants.h
@@ -3,12 +3,12 @@
 
 /* Constants required by XML parser */
 
-constexpr char* XML_BUS_GROUP_NODE_NAME = "bus_group";
-constexpr char* XML_BUS_NODE_NAME = "bus";
-constexpr char* XML_BUS_PORT_ATTRIBUTE_NAME = "name";
-constexpr char* XML_BUS_BIG_ENDIAN_ATTRIBUTE_NAME = "big_endian";
-constexpr char* XML_PIN_NODE_NAME = "pin";
-constexpr char* XML_PIN_INDEX_ATTRIBUTE_NAME = "id";
-constexpr char* XML_PIN_NAME_ATTRIBUTE_NAME = "name";
+constexpr const char* XML_BUS_GROUP_NODE_NAME = "bus_group";
+constexpr const char* XML_BUS_NODE_NAME = "bus";
+constexpr const char* XML_BUS_PORT_ATTRIBUTE_NAME = "name";
+constexpr const char* XML_BUS_BIG_ENDIAN_ATTRIBUTE_NAME = "big_endian";
+constexpr const char* XML_PIN_NODE_NAME = "pin";
+constexpr const char* XML_PIN_INDEX_ATTRIBUTE_NAME = "id";
+constexpr const char* XML_PIN_NAME_ATTRIBUTE_NAME = "name";
 
 #endif
