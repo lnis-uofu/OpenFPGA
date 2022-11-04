fee478585a1515d06327563c898ad1a669d86715
tangxifan <tangxifan@gmail.com>
2022-11-03 21:24:24 -0700

    [ci] fixed dependency errors due to ubuntu 20.04

--- .github/workflows/install_dependencies_run.sh
+++ .github/workflows/install_dependencies_run.sh
@@ -1,4 +1,4 @@
 apt-get install --no-install-recommends -y \
-libdatetime-perl libc6 libffi6 libgcc1 libreadline7 libstdc++6 \
+libdatetime-perl libc6 libffi-dev libgcc1 libreadline-dev libstdc++6 \
 libtcl8.6 python3.8 python3-pip zlib1g libbz2-1.0 \
 iverilog git rsync make curl wget tree python3.8-venv
