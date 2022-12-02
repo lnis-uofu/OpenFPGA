f1e39e7748a49433c14af7df57dfc7411939a243
tangxifan <tangxifan@gmail.com>
2022-12-02 11:12:44 -0800

    [ci] add tcl to dependency list

--- .github/workflows/install_dependencies_run.sh
+++ .github/workflows/install_dependencies_run.sh
@@ -1,4 +1,4 @@
 apt-get install --no-install-recommends -y \
 libdatetime-perl libc6 libffi-dev libgcc1 libreadline8 libstdc++6 \
-libtcl8.6 python3.8 python3-pip zlib1g libbz2-1.0 \
+libtcl8.6 tcl python3.8 python3-pip zlib1g libbz2-1.0 \
 iverilog git rsync make curl wget tree python3.8-venv
