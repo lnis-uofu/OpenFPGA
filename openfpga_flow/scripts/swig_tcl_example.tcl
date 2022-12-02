97c72c73f15aa75f3d26b5a65c26abb142bde026
tangxifan <tangxifan@gmail.com>
2022-12-02 11:43:46 -0800

    [test] add a small test to validate tcl integration

--- openfpga_flow/scripts/swig_tcl_example.tcl
+++ openfpga_flow/scripts/swig_tcl_example.tcl
@@ -1,3 +1,4 @@
+#! /bin/env tclsh
 # Load dynamic linked library
 load "openfpga_shell.so" openfpga_shell
 # Create a new Openfpga shell
@@ -5,4 +6,5 @@ std::OpenfpgaShell openfpga_shell
 # Run an command
 openfpga_shell run_command "help"
 # Finish the quit
-openfpga_shell run_command "exit"
+#openfpga_shell run_command "exit"
+#exit
