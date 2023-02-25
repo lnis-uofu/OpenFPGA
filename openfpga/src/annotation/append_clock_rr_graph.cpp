8be6e7d0a02fadc8ac259e234126e9bedd9e84aa
tangxifan <tangxifan@gmail.com>
2023-02-25 11:04:48 -0800

    [core] dev

--- openfpga/src/annotation/append_clock_rr_graph.cpp
+++ openfpga/src/annotation/append_clock_rr_graph.cpp
@@ -9,9 +9,37 @@
 /* begin namespace openfpga */
 namespace openfpga {
 
+/********************************************************************
+ * Estimate the number of clock nodes to be added for a given tile and clock structure
+ * For each layer/level of a clock network, we need 
+ * - the clock nodes are paired in INC and DEC directions
+ * - the number of clock nodes depend on the width of clock tree (number of clock signals)
+ * - Note that some layer only need CHANX or CHANY clock nodes since clock nodes cannot make turns in the same layer. 
+ *   For instance
+ *   - Layer 0: CHANX
+ *   - Layer 1: CHANY
+ *   - Layer 2: CHANX
+ *******************************************************************/
+static 
+size_t estimate_clock_rr_graph_num_nodes(const ClockNetwork& clk_ntwk,
+                                         const t_rr_type& chan_type) {
+  size_t num_nodes = 0;
+
+  
+
+  return num_nodes;
+}
+
+/********************************************************************
+ * Estimate the number of clock nodes to be added.
+ * Clock nodes are required by X-direction and Y-direction connection blocks
+ * which are in the type of CHANX and CHANY 
+ * Note that switch blocks do not require any new nodes but new edges
+ *******************************************************************/
 static 
 size_t estimate_clock_rr_graph_num_nodes(const DeviceGrid& grids,
-                                         const bool& through_channel) {
+                                         const bool& through_channel,
+                                         const ClockNetwork& clk_ntwk) {
   size_t num_nodes = 0;
   /* Check the number of CHANX nodes required */
   for (size_t iy = 0; iy < grids.height() - 1; ++iy) {
@@ -22,6 +50,8 @@ size_t estimate_clock_rr_graph_num_nodes(const DeviceGrid& grids,
         && (false == is_chanx_exist(grids, chanx_coord))) {
         continue;
       }
+      /* Estimate the routing tracks required by clock routing only */
+      num_nodes += estimate_clock_rr_graph_num_chan_nodes(clk_ntwk, CHANX);
     }
   }
 
@@ -33,6 +63,8 @@ size_t estimate_clock_rr_graph_num_nodes(const DeviceGrid& grids,
         && (false == is_chany_exist(grids, chany_coord))) {
         continue;
       }
+      /* Estimate the routing tracks required by clock routing only */
+      num_nodes += estimate_clock_rr_graph_num_chan_nodes(clk_ntwk, CHANY);
     }
   }
 
@@ -40,9 +72,12 @@ size_t estimate_clock_rr_graph_num_nodes(const DeviceGrid& grids,
 }
 
 /********************************************************************
- * Assign mapped blocks to grid locations
- * This is used by bitstream generator mainly as a fast look-up to
- * get mapped blocks with a given coordinate
+ * Append a programmable clock network to an existing routing resource graph
+ * This function will do the following jobs:
+ * - Estimate the number of clock nodes and pre-allocate memory
+ * - Add clock nodes
+ * - Build edges between clock nodes
+ * - Sanity checks
  *******************************************************************/
 int append_clock_rr_graph(DeviceContext& vpr_device_ctx,
                           const ClockNetwork& clk_ntwk, const bool& verbose) {
@@ -57,12 +92,19 @@ int append_clock_rr_graph(DeviceContext& vpr_device_ctx,
     return CMD_EXEC_SUCCESS;
   }
 
-  /* Estimate the number of nodes and pre-allocated */
+  /* TODO: report any clock structure we do not support yet! */
+
+  /* Estimate the number of nodes and pre-allocate */
   size_t orig_num_nodes = vpr_device_ctx.rr_graph.num_nodes();
-  size_t num_clock_nodes = estimate_clock_rr_graph_num_nodes(vpr_device_ctx.grid, vpr_device_ctx.arch->through_channel);
+  size_t num_clock_nodes = estimate_clock_rr_graph_num_nodes(vpr_device_ctx.grid, vpr_device_ctx.arch->through_channel, clk_ntwk);
   vpr_device_ctx.rr_graph_builder.reserve_nodes(num_clock_nodes + orig_num_nodes); 
 
+  /* TODO: Add clock nodes */
+
+  /* TODO: Add edges between clock nodes*/
   size_t num_clock_edges = 0;
+
+  /* TODO: Sanity checks */
 
   /* Report number of added clock nodes and edges */
   VTR_LOGV(
