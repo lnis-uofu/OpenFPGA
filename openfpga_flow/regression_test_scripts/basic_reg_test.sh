962ba67e366d3b8785ee336ce953b42d68fa0d21
tangxifan <tangxifan@gmail.com>
2023-06-23 14:47:21 -0700

    [test] adding new tests to validate fpga core wrapper naming rules

--- openfpga_flow/regression_test_scripts/basic_reg_test.sh
+++ openfpga_flow/regression_test_scripts/basic_reg_test.sh
@@ -17,7 +17,9 @@ run-task basic_tests/source_command/source_file $@
 
 echo -e "Testing testbenches using fpga core wrapper"
 run-task basic_tests/full_testbench/fpga_core_wrapper $@
+run-task basic_tests/full_testbench/fpga_core_wrapper_naming_rules $@
 run-task basic_tests/preconfig_testbench/fpga_core_wrapper $@
+run-task basic_tests/preconfig_testbench/fpga_core_wrapper_naming_rules $@
 
 echo -e "Testing configuration chain of a K4N4 FPGA";
 run-task basic_tests/full_testbench/configuration_chain $@
