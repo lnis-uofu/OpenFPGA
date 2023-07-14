bb36e72c7775e279b1cdbf1ee027d4f4bb7f84a7
tangxifan <tangxifan@gmail.com>
2023-07-14 10:27:24 -0700

    [ci] now use apt update before installing dependencies

--- .github/workflows/install_dependencies_run_ubuntu22p04.sh
+++ .github/workflows/install_dependencies_run_ubuntu22p04.sh
@@ -1,3 +1,5 @@
+# Update as required by some packages
+apt-get update
 apt-get install --no-install-recommends -y \
 libdatetime-perl libc6 libffi-dev libgcc1 libreadline8 libstdc++6 \
 libtcl8.6 tcl python3-pip zlib1g libbz2-1.0 \
