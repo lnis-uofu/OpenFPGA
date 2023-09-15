e5bc936144d1b456874bec88bb9356b6efdcf3a7
tangxifan <tangxifan@gmail.com>
2023-09-15 16:19:10 -0700

    [lib] developing io

---
+++ libs/libnamemanager/src/io/write_xml_module_name_map.h
@@ -0,0 +1,20 @@
+#ifndef WRITE_XML_MODULE_NAME_MAP_H
+#define WRITE_XML_MODULE_NAME_MAP_H
+
+/********************************************************************
+ * Include header files that are required by function declaration
+ *******************************************************************/
+#include <fstream>
+
+#include "module_name_map.h"
+
+/********************************************************************
+ * Function declaration
+ *******************************************************************/
+namespace openfpga {  // Begin namespace openfpga
+
+int write_xml_module_name_map(const char* fname, const ModuleNameMap& module_name_map);
+
+}  // End of namespace openfpga
+
+#endif
