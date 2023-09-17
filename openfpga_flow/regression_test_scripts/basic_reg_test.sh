0ef1e0bde5e2b2df09ea804a13b3c4c929ceec8a
tangxifan <tangxifan@gmail.com>
2023-09-17 13:29:12 -0700

    [test] add a new test to validate renaming rules

--- openfpga_flow/regression_test_scripts/basic_reg_test.sh
+++ openfpga_flow/regression_test_scripts/basic_reg_test.sh
@@ -204,6 +204,7 @@ run-task basic_tests/group_config_block/group_config_block_homo_fabric_tile_glob
 
 echo -e "Module naming";
 run-task basic_tests/module_naming/using_index $@
+run-task basic_tests/module_naming/renaming_rules $@
 
 echo -e "Testing global port definition from tiles";
 run-task basic_tests/global_tile_ports/global_tile_clock $@
