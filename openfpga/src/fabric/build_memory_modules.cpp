76a4b8a82ba97d94d4040f14e3c6aa16e01e5b38
tangxifan <tangxifan@gmail.com>
2023-10-20 21:13:37 -0700

    [core] remove the prefix of grouped memory blocks

--- openfpga/src/fabric/build_memory_modules.cpp
+++ openfpga/src/fabric/build_memory_modules.cpp
@@ -1510,7 +1510,7 @@ int add_physical_memory_module(ModuleManager& module_manager,
     return CMD_EXEC_SUCCESS;
   }
   std::string phy_mem_module_name = generate_physical_memory_module_name(
-    module_manager.module_name(curr_module), module_num_config_bits);
+    std::string(), module_num_config_bits);
   VTR_LOGV(verbose, "Adding memory group module '%s' as a child to '%s'...\n",
            phy_mem_module_name.c_str(),
            module_manager.module_name(curr_module).c_str());
