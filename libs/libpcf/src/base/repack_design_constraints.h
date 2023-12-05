aa51b6d3881ca286351c6bb1cc23911ebba183b9
Yitian4Debug <97993988+Yitian4Debug@users.noreply.github.com>
2023-12-05 09:40:25 -0800

    Update repack_design_constraints.h

--- libs/libpcf/src/base/repack_design_constraints.h
+++ libs/libpcf/src/base/repack_design_constraints.h
@@ -69,7 +69,6 @@ class RepackDesignConstraints {
   std::string net(
     const RepackDesignConstraintId& repack_design_constraint_id) const;
 
-  std::set<std::string> ignore_net_on_pin(const std::string& net_name) const;
   /* Find a constrained net */
   std::string find_constrained_pin_net(const std::string& pb_type,
                                        const openfpga::BasicPort& pin) const;
@@ -98,9 +97,6 @@ class RepackDesignConstraints {
   /* Set the net to be constrained */
   void set_net(const RepackDesignConstraintId& repack_design_constraint_id,
                const std::string& net);
-
-  void set_ignore_net_pin_map_(const std::string& net_name,
-                               const std::string pin_ctx);
 
  public: /* Public invalidators/validators */
   bool valid_design_constraint_id(
@@ -139,8 +135,6 @@ class RepackDesignConstraints {
   /* Nets to constraint */
   vtr::vector<RepackDesignConstraintId, std::string>
     repack_design_constraint_nets_;
-
-  std::map<std::string, std::set<std::string>> ignore_net_pin_map_;
 };
 
 #endif
