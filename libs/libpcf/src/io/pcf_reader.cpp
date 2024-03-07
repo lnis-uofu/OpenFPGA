135dbb783c13eb064bae43abe41861e95e51cf1f
ubuntu <jrlin@rapid-flex.com>
2024-03-06 20:03:58 -0800

    reformat the code

--- libs/libpcf/src/io/pcf_reader.cpp
+++ libs/libpcf/src/io/pcf_reader.cpp
@@ -57,19 +57,17 @@ int read_pcf(const char* fname, PcfData& pcf_data) {
           pcf_data.set_io_pin(io_id, pin_name);
         } else if (word[0] == COMMENT) {  // if it's a comment
           break;  // or ignore the full line comment and move on
+        } else if (word.find("set_clk") == 0 || word.find("set_reset") == 0) {
+          /* set_clk and set_rest are known commands for Arkangel, disable the
+           * error message for these two commands when call read_pcf function
+           */
+          break;
         } else {
-          if (word.find("set_clk") == 0 || word.find("set_reset") == 0) {
-            /* set_clk and set_rest are known commands for Arkangel, disable the
-             * error message for these two commands when call read_pcf function
-             */
-            break;
-          } else {
-            /* Reach unknown command for OpenFpga, error out */
-            VTR_LOG_ERROR("Unknown command '%s'!\n", word.c_str());
-            num_err++;
-            break;  // and move onto next line. without this, it will accept
-                    // more following values on this line
-          }
+          /* Reach unknown command for OpenFpga, error out */
+          VTR_LOG_ERROR("Unknown command '%s'!\n", word.c_str());
+          num_err++;
+          break;  // and move onto next line. without this, it will accept
+                  // more following values on this line
         }
       }
     }
