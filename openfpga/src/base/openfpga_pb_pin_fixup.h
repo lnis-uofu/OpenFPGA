812686d1694cbe85c03f8e39ec20cef35fede933
tangxifan <tangxifan@gmail.com>
2024-08-13 22:36:37 -0700

    [core] support global net fixup in pb pin fixup

--- openfpga/src/base/openfpga_pb_pin_fixup.h
+++ openfpga/src/base/openfpga_pb_pin_fixup.h
@@ -14,7 +14,7 @@
 /* begin namespace openfpga */
 namespace openfpga {
 
-void update_pb_pin_with_post_routing_results(
+int update_pb_pin_with_post_routing_results(
   const DeviceContext& device_ctx, const ClusteringContext& clustering_ctx,
   const PlacementContext& placement_ctx,
   const VprRoutingAnnotation& vpr_routing_annotation,
