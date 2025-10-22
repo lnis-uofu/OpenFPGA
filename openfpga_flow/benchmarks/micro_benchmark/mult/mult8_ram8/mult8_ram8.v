730fb7c998dc4b6d80fb0d17a2ac3af91787e618
tangxifan <tangxifan@gmail.com>
2025-10-22 12:42:41 -0700

    [test] debug

--- openfpga_flow/benchmarks/micro_benchmark/mult/mult8_ram8/mult8_ram8.v
+++ openfpga_flow/benchmarks/micro_benchmark/mult/mult8_ram8/mult8_ram8.v
@@ -3,14 +3,19 @@
 //  Author:        Tarachand Pagarani
 //-------------------------------------------------------
 
-module mult8_ram8(a, b, clk, wen, ren, waddr, raddr, out);
+module mult8_ram8(a, b, clk, waddr, raddr, out);
 input [7:0] a, b;
-input clk, wen, ren;
+input clk;
 input [6:0] waddr;
 input [6:0] raddr;
 output [15:0] out;
 
 wire [15:0] mult_y;
+wire wen;
+wire ren;
+
+assign wen = 1'b1;
+assign ren = 1'b1;
 
 assign mult_y = a * b;
 
