40ca8bc7774163e02c94a0a208f56b9d852a62b1
tangxifan <tangxifan@gmail.com>
2025-12-02 15:39:01 -0800

    [core] debug

--- cmake/modules/OpenfpgaDeps.cmake
+++ cmake/modules/OpenfpgaDeps.cmake
@@ -9,7 +9,18 @@ set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 #    COMPONENT dep_package
 #)
 find_package(OpenSSL REQUIRED COMPONENTS SSL Crypto)
-install(FILES ${OPENSSL_LIBRARIES}
-    DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    COMPONENT dep_package
-)
+message("Found .so file at ${OPENSSL_LIBRARIES}")
+foreach (src ${OPENSSL_LIBRARIES})
+    if(IS_SYMLINK ${src})
+        file(REAL_PATH ${src} TARGET_PATH)
+        message("Follow the real path to .so file: ${src} ->  ${TARGET_PATH}")
+        install(FILES ${TARGET_PATH}}
+            DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            COMPONENT dep_package
+        )
+    endif()
+    install(FILES ${OPENSSL_LIBRARIES}
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        COMPONENT dep_package
+    )
+endforeach()
