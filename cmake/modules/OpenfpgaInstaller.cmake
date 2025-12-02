7341d20e696208de7dd552d8f75ce148e5d4fb15
tangxifan <tangxifan@gmail.com>
2025-12-01 16:38:24 -0800

    [core] debug

--- cmake/modules/OpenfpgaInstaller.cmake
+++ cmake/modules/OpenfpgaInstaller.cmake
@@ -28,3 +28,39 @@ install(FILES "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE"
         DESTINATION "."
         COMPONENT openfpga_package
 )
+
+# Include runtime libraries in the packages
+install(CODE [[
+  function(install_library_with_deps LIBRARY)
+    file(INSTALL
+      DESTINATION "${CMAKE_INSTALL_PREFIX}/lib"
+      TYPE SHARED_LIBRARY
+      FOLLOW_SYMLINK_CHAIN
+      FILES "${LIBRARY}"
+    )
+    file(GET_RUNTIME_DEPENDENCIES
+      LIBRARIES ${LIBRARY}
+      RESOLVED_DEPENDENCIES_VAR RESOLVED_DEPS
+      UNRESOLVED_DEPENDENCIES_VAR UNRESOLVED_DEPS
+    )
+    foreach(FILE ${RESOLVED_DEPS})
+      if(NOT IS_SYMLINK ${FILE})
+        install_library_with_deps(${FILE})
+      endif()
+    endforeach()
+    foreach(FILE ${UNRESOLVED_DEPS})
+      message(STATUS "Unresolved from ${LIBRARY}: ${FILE}")
+    endforeach()
+  endfunction()
+  file(GET_RUNTIME_DEPENDENCIES
+    EXECUTABLES  $<TARGET_FILE:myexecutable>
+    RESOLVED_DEPENDENCIES_VAR RESOLVED_DEPS
+    UNRESOLVED_DEPENDENCIES_VAR UNRESOLVED_DEPS
+  )
+  foreach(FILE ${RESOLVED_DEPS})
+    install_library_with_deps(${FILE})
+  endforeach()
+  foreach(FILE ${UNRESOLVED_DEPS})
+    message(STATUS "Unresolved: ${FILE}")
+  endforeach()
+]])
