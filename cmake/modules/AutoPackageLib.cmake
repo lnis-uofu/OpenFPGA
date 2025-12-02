b9a9343d09399c2994038ceac0eac6280dfdfcfb
tangxifan <tangxifan@gmail.com>
2025-12-02 15:54:41 -0800

    [core] debug

---
+++ cmake/modules/AutoPackageLib.cmake
@@ -0,0 +1,17 @@
+function(auto_package_lib file_list)
+  foreach (src ${file_list})
+      if(IS_SYMLINK ${src})
+          file(REAL_PATH ${src} TARGET_PATH)
+          message("Follow the real path to .so file: ${src} ->  ${target}")
+          install(FILES ${target}
+              DESTINATION ${CMAKE_INSTALL_LIBDIR}
+              COMPONENT dep_package
+          )
+      endif()
+      install(FILES ${file_list}
+          DESTINATION ${CMAKE_INSTALL_LIBDIR}
+          COMPONENT dep_package
+      )
+endforeach()
+
+endfunction(auto_package_lib)
--- cmake/modules/OpenfpgaDeps.cmake
+++ cmake/modules/OpenfpgaDeps.cmake
@@ -1,26 +1,11 @@
 # Special for Yosys, which does not use CMake to install. Do manual install configuration here
 include(GNUInstallDirs)
+include(AutoPackageLib)
 
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 
 # Install dep libs
-#install(FILES ${OPENSSL_INCLUDE_DIRS}/openssl/ssl.h
-#    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-#    COMPONENT dep_package
-#)
+# SSL
 find_package(OpenSSL REQUIRED COMPONENTS SSL Crypto)
 message("Found .so file at ${OPENSSL_LIBRARIES}")
-foreach (src ${OPENSSL_LIBRARIES})
-    if(IS_SYMLINK ${src})
-        file(REAL_PATH ${src} TARGET_PATH)
-        message("Follow the real path to .so file: ${src} ->  ${TARGET_PATH}")
-        install(FILES ${TARGET_PATH}
-            DESTINATION ${CMAKE_INSTALL_LIBDIR}
-            COMPONENT dep_package
-        )
-    endif()
-    install(FILES ${OPENSSL_LIBRARIES}
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}
-        COMPONENT dep_package
-    )
-endforeach()
+auto_package(${OPENSSL_LIBRARIES})
