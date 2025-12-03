89af3d384b3316463717ffd925e930a9c499649b
tangxifan <tangxifan@gmail.com>
2025-12-02 16:17:49 -0800

    [core] debug

--- cmake/modules/AutoPackageLib.cmake
+++ cmake/modules/AutoPackageLib.cmake
@@ -1,19 +1,21 @@
 # Automatically follow the symbolic link to an .so file
 # and add it to installer. Good for collecting dependencies
 function(auto_package_lib file_list)
-  foreach (src ${file_list})
-      if(IS_SYMLINK ${src})
-          file(REAL_PATH ${src} target)
-          message("Follow the real path to .so file: ${src} ->  ${target}")
-          install(FILES ${target}
-              DESTINATION ${CMAKE_INSTALL_LIBDIR}
-              COMPONENT dep_package
-          )
-      endif()
-      install(FILES ${file_list}
-          DESTINATION ${CMAKE_INSTALL_LIBDIR}
-          COMPONENT dep_package
-      )
-endforeach()
-
+    if (NOT "${file_list}" STREQUAL "")
+        message("Found .so file at ${file_list}")
+        foreach (src ${file_list})
+            if(IS_SYMLINK ${src})
+                file(REAL_PATH ${src} target)
+                message("Follow the real path to .so file: ${src} ->  ${target}")
+                install(FILES ${target}
+                    DESTINATION ${CMAKE_INSTALL_LIBDIR}
+                    COMPONENT dep_package
+                )
+            endif()
+            install(FILES ${file_list}
+                DESTINATION ${CMAKE_INSTALL_LIBDIR}
+                COMPONENT dep_package
+            )
+        endforeach()
+    endif()
 endfunction(auto_package_lib)
--- cmake/modules/OpenfpgaDeps.cmake
+++ cmake/modules/OpenfpgaDeps.cmake
@@ -7,5 +7,20 @@ set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 # Install dep libs
 # SSL
 find_package(OpenSSL REQUIRED COMPONENTS SSL Crypto)
-message("Found .so file at ${OPENSSL_LIBRARIES}")
 auto_package_lib(${OPENSSL_LIBRARIES})
+
+# BOOST
+find_package(Boost 1.87.0 CONFIG QUIET)
+find_package(Boost REQUIRED COMPONENTS date_time unit_test_framework system filesystem)
+auto_package_lib(${Boost_LIBRARIES})
+
+# ZLIB
+find_package(ZLIB REQUIRED)
+auto_package_lib(${ZLIB_LIBRARIES})
+
+find_package(FLEX REQUIRED)
+auto_package_lib(${FLEX_LIBRARIES})
+
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(LIBFFI REQUIRED libffi)
+auto_package_lib(${LIBFFI_LIBRARIES})
