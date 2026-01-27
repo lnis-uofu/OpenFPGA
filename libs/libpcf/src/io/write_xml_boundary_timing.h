7d894cffe53466d8868dce3f8828acf99c72fca1
treelin611 <jrlin@rapid-flex.com>
2026-01-27 16:38:16 +0800

    update write boundary timing

--- libs/libpcf/src/io/write_xml_boundary_timing.h
+++ libs/libpcf/src/io/write_xml_boundary_timing.h
@@ -11,7 +11,11 @@
 
 /* Begin namespace openfpga */
 namespace openfpga {
-
+constexpr const char* XML_BOUNDARY_TIMING_ROOT_NAME = "boundary_timing";
+constexpr const char* XML_BOUNDARY_TIMING_TREE_NODE = "pin";
+constexpr const char* XML_BOUNDARY_TIMING_TREE_NODE_PIN_NAME = "name";
+constexpr const char* XML_BOUNDARY_TIMING_TREE_NODE_PIN_MIN_DELAY = "min";
+constexpr const char* XML_BOUNDARY_TIMING_TREE_NODE_PIN_MAX_DELAY = "max";
 int write_xml_boundary_timing(const char* fname,
                               const BoundaryTiming& bdy_timing);
 } /* End namespace openfpga*/
