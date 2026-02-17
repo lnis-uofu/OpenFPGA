4f76766a7eb1b65a0518141b6b420c6cded42c80
tangxifan <tangxifan@gmail.com>
2026-02-17 19:07:00 +0800

    [ci] install swig on win

--- .github/workflows/install_dependencies_build_win.ps1
+++ .github/workflows/install_dependencies_build_win.ps1
@@ -1,1 +1,9 @@
 # Install dependencies for source code build on powershell
+
+# Download and extract the swigwin package
+(New-Object System.Net.WebClient).DownloadFile("https://prdownloads.sourceforge.net", "swigwin-4.1.0.zip")
+Expand-Archive -Path swigwin-4.1.0.zip -DestinationPath .
+# Add the directory containing the swig.exe to the PATH
+$env:Path += ";$PWD\swigwin-4.1.0"
+# Make the updated PATH available to subsequent steps in the job
+echo "::add-path::$PWD\swigwin-4.1.0"
