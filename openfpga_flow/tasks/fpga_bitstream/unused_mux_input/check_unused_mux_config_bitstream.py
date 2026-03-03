a80ed9d44fce85b02833b40433558838b76b9732
ganeshgore <ggore@quicklogic.com>
2026-03-02 20:03:02 -0800

    Fixed regression script

--- openfpga_flow/tasks/fpga_bitstream/unused_mux_input/check_unused_mux_config_bitstream.py
+++ openfpga_flow/tasks/fpga_bitstream/unused_mux_input/check_unused_mux_config_bitstream.py
@@ -6,6 +6,7 @@ parser.add_argument("--default_bitstream", help="Path to default bitstream file"
 parser.add_argument("--auto_bitstream", help="Path to auto bitstream file")
 parser.add_argument("--first_bitstream", help="Path to first bitstream file")
 parser.add_argument("--last_bitstream", help="Path to last bitstream file")
+# parser.add_argument("--unused_input_bitstream", help="Path to unused_input bitstream file")
 args = parser.parse_args()
 
 print("Validated the bitstream generated for the unused mux input")
@@ -13,12 +14,12 @@ print(f"Default bitstream: {args.default_bitstream}")
 print(f"Auto bitstream: {args.auto_bitstream}")
 print(f"First bitstream: {args.first_bitstream}")
 print(f"Last bitstream: {args.last_bitstream}")
+# print(f"Unused input bitstream: {args.unused_input_bitstream}")
 
 
 def get_unused_muxes(root):
     unused_muxes = {}
     interconnect_muxes = []
-    unused_muxes_bits = []
     for bitstream_block in root.findall(".//bitstream_block"):
         bitstream = bitstream_block.find("bitstream")
         if bitstream is not None:
@@ -60,12 +61,16 @@ print(f"Found {len(interconnect_muxes)} interconnect muxes in the default bitstr
 tree = ET.parse(args.auto_bitstream)
 root = tree.getroot()
 auto_unused_muxes, auto_interconnect_muxes = get_unused_muxes(root)
-assert set(unused_muxes.keys()) == set(auto_unused_muxes.keys())
-assert set(interconnect_muxes) == set(auto_interconnect_muxes)
+if set(unused_muxes.keys()) != set(auto_unused_muxes.keys()):
+    exit(1)
+if set(interconnect_muxes) != set(auto_interconnect_muxes):
+    exit(1)
 
 for path in unused_muxes.keys():
     default_bit, last_bit = unused_muxes[path], auto_unused_muxes[path]
-    assert default_bit[1] == last_bit[1], f"Input nets mismatch for mux {path}"
+    if default_bit[1] != last_bit[1]:
+        print(f"Bits Mismatch for mux {path}")
+        exit(1)
 
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
@@ -75,27 +80,45 @@ for path in unused_muxes.keys():
 tree = ET.parse(args.first_bitstream)
 root = tree.getroot()
 first_unused_muxes, first_interconnect_muxes = get_unused_muxes(root)
-assert set(unused_muxes.keys()) == set(first_unused_muxes.keys())
+if set(unused_muxes.keys()) != set(first_unused_muxes.keys()):
+    exit(1)
 
 for path in unused_muxes.keys():
     default_bit, first_bit = unused_muxes[path], first_unused_muxes[path]
     expected_bits = ['1'] * len(first_bit[1])
-    assert (
-        first_bit[1] == expected_bits
-    ), f"Input nets mismatch for mux {path} {first_bit[1]} != {expected_bits}"
+    if first_bit[1] != expected_bits:
+        print(f"Bits Mismatch for mux {path} {first_bit[1]} != {expected_bits}")
+        exit(1)
 
 
-# # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-# # Validate the unused muxes in the last bitstream
-# # All the unused muxes bit should be set to 0
-# # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-# tree = ET.parse(args.last_bitstream)
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# Validate the unused muxes in the last bitstream
+# All the unused muxes bit should be set to 0
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+tree = ET.parse(args.last_bitstream)
+root = tree.getroot()
+last_unused_muxes, last_interconnect_muxes = get_unused_muxes(root)
+assert set(unused_muxes.keys()) == set(last_unused_muxes.keys())
+
+for path in unused_muxes.keys():
+    default_bit, last_bit = unused_muxes[path], last_unused_muxes[path]
+    expected_bits = ["0"] * len(last_bit[1])
+    if last_bit[1] != expected_bits:
+        print(f"Bits Mismatch for mux {path} {last_bit[1]} != {expected_bits}")
+        exit(1)
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# NotImplemented: Validate the unused muxes in the last bitstream
+# All the unused muxes bit should be set to 0, and the interconnect muxes should
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# tree = ET.parse(args.unused_input_bitstream)
 # root = tree.getroot()
-# last_unused_muxes, last_interconnect_muxes = get_unused_muxes(root)
-# assert set(unused_muxes.keys()) == set(last_unused_muxes.keys())
 
 # for path in unused_muxes.keys():
-#     default_bit, last_bit = unused_muxes[path], last_unused_muxes[path]
-#     print(f"Default bit: {default_bit[1]}, Last bit: {last_bit[1]} Path: {path}")
-
-# #
+#     input_nets, bitstream = unused_muxes[path]
+#     first_unmapped = input_nets.index("unmapped")
+#     expected_bit = f"{bin(first_unmapped)[2:].zfill(len(bitstream))}"
+#     expected_bit_str = list(expected_bit)
+#     if bitstream != expected_bit_str:
+#         print(f"Bits Mismatch for mux {path} {bitstream} != {expected_bit_str}")
+#         exit(1)
