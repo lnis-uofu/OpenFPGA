2c6f001791642f473e957e74ae7e781f74732a20
ganeshgore <ggore@quicklogic.com>
2026-03-03 23:45:51 -0800

    Updated custom graph

--- openfpga_flow/tasks/basic_tests/custom_rrgraph/custom_rrgraph_group_routing/SRC/sub_module/mux_primitives.v
+++ openfpga_flow/tasks/basic_tests/custom_rrgraph/custom_rrgraph_group_routing/SRC/sub_module/mux_primitives.v
@@ -7,76 +7,6 @@
 //----- Default net type -----
 `default_nettype none
 
-// ----- Verilog module for mux_2level_tapbuf_basis_input5_mem5 -----
-module mux_2level_tapbuf_basis_input5_mem5(in,
-                                           mem,
-                                           mem_inv,
-                                           out);
-//----- INPUT PORTS -----
-input [4:0] in;
-//----- INPUT PORTS -----
-input [4:0] mem;
-//----- INPUT PORTS -----
-input [4:0] mem_inv;
-//----- OUTPUT PORTS -----
-output [0:0] out;
-
-//----- BEGIN wire-connection ports -----
-//----- END wire-connection ports -----
-
-
-//----- BEGIN Registered ports -----
-//----- END Registered ports -----
-
-
-
-// ----- BEGIN Local short connections -----
-// ----- END Local short connections -----
-// ----- BEGIN Local output short connections -----
-// ----- END Local output short connections -----
-
-	TGATE TGATE_0_ (
-		.A(in[0]),
-		.S(mem[0]),
-		.SI(mem_inv[0]),
-		.Y(out));
-
-	TGATE TGATE_1_ (
-		.A(in[1]),
-		.S(mem[1]),
-		.SI(mem_inv[1]),
-		.Y(out));
-
-	TGATE TGATE_2_ (
-		.A(in[2]),
-		.S(mem[2]),
-		.SI(mem_inv[2]),
-		.Y(out));
-
-	TGATE TGATE_3_ (
-		.A(in[3]),
-		.S(mem[3]),
-		.SI(mem_inv[3]),
-		.Y(out));
-
-	TGATE TGATE_4_ (
-		.A(in[4]),
-		.S(mem[4]),
-		.SI(mem_inv[4]),
-		.Y(out));
-
-endmodule
-// ----- END Verilog module for mux_2level_tapbuf_basis_input5_mem5 -----
-
-//----- Default net type -----
-`default_nettype wire
-
-
-
-
-//----- Default net type -----
-`default_nettype none
-
 // ----- Verilog module for mux_2level_tapbuf_basis_input7_mem7 -----
 module mux_2level_tapbuf_basis_input7_mem7(in,
                                            mem,
@@ -159,17 +89,17 @@ endmodule
 //----- Default net type -----
 `default_nettype none
 
-// ----- Verilog module for mux_2level_tapbuf_basis_input4_mem4 -----
-module mux_2level_tapbuf_basis_input4_mem4(in,
+// ----- Verilog module for mux_2level_tapbuf_basis_input9_mem9 -----
+module mux_2level_tapbuf_basis_input9_mem9(in,
                                            mem,
                                            mem_inv,
                                            out);
 //----- INPUT PORTS -----
-input [3:0] in;
+input [8:0] in;
 //----- INPUT PORTS -----
-input [3:0] mem;
+input [8:0] mem;
 //----- INPUT PORTS -----
-input [3:0] mem_inv;
+input [8:0] mem_inv;
 //----- OUTPUT PORTS -----
 output [0:0] out;
 
@@ -211,8 +141,38 @@ output [0:0] out;
 		.SI(mem_inv[3]),
 		.Y(out));
 
+	TGATE TGATE_4_ (
+		.A(in[4]),
+		.S(mem[4]),
+		.SI(mem_inv[4]),
+		.Y(out));
+
+	TGATE TGATE_5_ (
+		.A(in[5]),
+		.S(mem[5]),
+		.SI(mem_inv[5]),
+		.Y(out));
+
+	TGATE TGATE_6_ (
+		.A(in[6]),
+		.S(mem[6]),
+		.SI(mem_inv[6]),
+		.Y(out));
+
+	TGATE TGATE_7_ (
+		.A(in[7]),
+		.S(mem[7]),
+		.SI(mem_inv[7]),
+		.Y(out));
+
+	TGATE TGATE_8_ (
+		.A(in[8]),
+		.S(mem[8]),
+		.SI(mem_inv[8]),
+		.Y(out));
+
 endmodule
-// ----- END Verilog module for mux_2level_tapbuf_basis_input4_mem4 -----
+// ----- END Verilog module for mux_2level_tapbuf_basis_input9_mem9 -----
 
 //----- Default net type -----
 `default_nettype wire
@@ -289,6 +249,70 @@ output [0:0] out;
 
 endmodule
 // ----- END Verilog module for mux_2level_tapbuf_basis_input6_mem6 -----
+
+//----- Default net type -----
+`default_nettype wire
+
+
+
+
+//----- Default net type -----
+`default_nettype none
+
+// ----- Verilog module for mux_2level_tapbuf_basis_input4_mem4 -----
+module mux_2level_tapbuf_basis_input4_mem4(in,
+                                           mem,
+                                           mem_inv,
+                                           out);
+//----- INPUT PORTS -----
+input [3:0] in;
+//----- INPUT PORTS -----
+input [3:0] mem;
+//----- INPUT PORTS -----
+input [3:0] mem_inv;
+//----- OUTPUT PORTS -----
+output [0:0] out;
+
+//----- BEGIN wire-connection ports -----
+//----- END wire-connection ports -----
+
+
+//----- BEGIN Registered ports -----
+//----- END Registered ports -----
+
+
+
+// ----- BEGIN Local short connections -----
+// ----- END Local short connections -----
+// ----- BEGIN Local output short connections -----
+// ----- END Local output short connections -----
+
+	TGATE TGATE_0_ (
+		.A(in[0]),
+		.S(mem[0]),
+		.SI(mem_inv[0]),
+		.Y(out));
+
+	TGATE TGATE_1_ (
+		.A(in[1]),
+		.S(mem[1]),
+		.SI(mem_inv[1]),
+		.Y(out));
+
+	TGATE TGATE_2_ (
+		.A(in[2]),
+		.S(mem[2]),
+		.SI(mem_inv[2]),
+		.Y(out));
+
+	TGATE TGATE_3_ (
+		.A(in[3]),
+		.S(mem[3]),
+		.SI(mem_inv[3]),
+		.Y(out));
+
+endmodule
+// ----- END Verilog module for mux_2level_tapbuf_basis_input4_mem4 -----
 
 //----- Default net type -----
 `default_nettype wire
