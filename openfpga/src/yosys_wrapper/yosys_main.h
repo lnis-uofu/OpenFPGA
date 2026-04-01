f8c9bf95da474f3012a7b2ca459c073d12fbaaaf
Ganesh Gore <ggore@quicklogic.com>
2026-04-01 10:46:09 -0700

    Adding support to run yosys from openfpga shell
    [Yosys wrapper]

---
+++ openfpga/src/yosys_wrapper/yosys_main.h
@@ -0,0 +1,11 @@
+#ifndef YOSYS_MAIN_H
+#define YOSYS_MAIN_H
+
+/* Begin namespace openfpga */
+namespace openfpga {
+
+int yosys_wrapper(int argc, char** argv);
+
+} /* End namespace openfpga */
+
+#endif
