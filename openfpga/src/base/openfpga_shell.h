a613fb4906e091418b0bd78f2fd5995e015f89c8
Ganesh Gore <ggore@quicklogic.com>
2026-04-11 08:52:56 -0700

    Added app options and fixed initial sync up

--- openfpga/src/base/openfpga_shell.h
+++ openfpga/src/base/openfpga_shell.h
@@ -5,6 +5,7 @@
 
 #include "openfpga_context.h"
 #include "shell.h"
+#include "vpr_types.h"
 
 /********************************************************************
  * This is a shell class built on top of the general-purpose shell
@@ -16,7 +17,18 @@ class OpenfpgaShell {
  public: /* Contructors */
   OpenfpgaShell();
 
+ private: /* Mutators */
+  // Add default options for OpenFPGA applications, which will be used in the
+  // command execution
+  void setup_default_app_options();
+
  public: /* Mutators */
+  // Sync the options in VPR setup to the app options in the shell, so that they
+  // can be used by commands in the shell
+  void sync_vpr_setup_to_app_options(t_vpr_setup* vpr_setup,
+                                     openfpga::Shell<OpenfpgaContext>& shell);
+  void setupvpr_from_ofshell();
+
   /* Execute a specific command with options in a line, which is available in
    * the shell Note that running a command will be based on the current status
    * of data storage. The data storage is impacted by previous commands that
@@ -42,6 +54,7 @@ class OpenfpgaShell {
  private: /* Internal data */
   openfpga::Shell<OpenfpgaContext> shell_;
   OpenfpgaContext openfpga_ctx_;
+  t_vpr_setup vpr_setup = t_vpr_setup();
 };
 
 #endif
