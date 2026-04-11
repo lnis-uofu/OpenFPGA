afb4eb68747cc7b6ec9670c8e412f1d23f64a25f
Ganesh Gore <ggore@quicklogic.com>
2026-04-11 08:52:56 -0700

    Added app options and fixed initial sync up

---
+++ openfpga_flow/regression_test_scripts/app_options.test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+source openfpga.sh
+
+###############################################
+# OpenFPGA Shell with VPR8
+##############################################
+
+run_openfpga_command() {
+    local command="$1"
+    ${OPENFPGA_PATH}/build/openfpga/openfpga -x "${command}; exit;"
+    if [ $? -ne 0 ]; then
+        echo "Command '${command}' failed in OpenFPGA Shell with VPR8"
+        exit 1
+    fi
+}
+
+echo -e "Testing app options commands in OpenFPGA Shell with VPR8"
+
+run_openfpga_command "report_app_option"
+run_openfpga_command "set_app_option -n atom.const_gen_inference -v comb_seq; report_app_option -n atom.const_gen_inference"
