8c816dfb2d6db1e663faa8a36203adf479042929
Ganesh Gore <ggore@quicklogic.com>
2026-04-11 18:09:08 -0700

    fixed read arch command

--- openfpga_flow/regression_test_scripts/app_options.test.sh
+++ openfpga_flow/regression_test_scripts/app_options.test.sh
@@ -19,4 +19,11 @@ run_openfpga_command() {
 echo -e "Testing app options commands in OpenFPGA Shell with VPR8"
 
 run_openfpga_command "report_app_option"
-run_openfpga_command "set_app_option -n atom.const_gen_inference -v comb_seq; report_app_option -n atom.const_gen_inference"
+
+run_openfpga_command "\
+set_app_option -n atom.const_gen_inference -v comb_seq; \
+report_app_option -n atom.const_gen_inference"
+
+
+run_openfpga_command "\
+read_vpr_arch -f ${OPENFPGA_PATH}/openfpga_flow/vpr_arch/k4_frac_N4_tileable_40nm_custrrg.xml"
