b54db0a9adeb8aac41874c82ce598e03511d9165
Ganesh Gore <ggore@quicklogic.com>
2026-04-13 23:29:05 -0700

    Added placer sync options

--- openfpga/src/vpr_wrapper/vpr_shell_utils.h
+++ openfpga/src/vpr_wrapper/vpr_shell_utils.h
@@ -16,6 +16,9 @@ void sync_vpr_setup_to_app_options(t_vpr_setup& vpr_setup,
 void shell_setup_packer_opts(t_vpr_setup* vpr_setup,
                              openfpga::Shell<OpenfpgaContext>* shell);
 
+void shell_setup_placer_opts(t_vpr_setup* vpr_setup,
+                             openfpga::Shell<OpenfpgaContext>* shell);
+
 int validate_vpr_arch_types(
   const std::string& arch_file_name,
   const std::vector<t_physical_tile_type>& physical_tile_types,
