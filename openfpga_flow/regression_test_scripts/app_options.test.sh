a4eb7245075df2e9ad31b7331e3f08a41211e1b4
Ganesh Gore <ggore@quicklogic.com>
2026-04-18 21:02:50 -0700

    Fixed exit code

--- openfpga_flow/regression_test_scripts/app_options.test.sh
+++ openfpga_flow/regression_test_scripts/app_options.test.sh
@@ -1,46 +1,62 @@
 #!/bin/bash
 
-set -e
 source openfpga.sh
 
 ###############################################
 # OpenFPGA Shell with VPR8
 ##############################################
 
-run_openfpga_command() {
-    local command="$1"
-    ${OPENFPGA_PATH}/build/openfpga/openfpga -x "${command}; exit;"
+run_tcl_command() {
+    local test_name="$1"
+    local command="$2"
+    mkdir -p ${OPENFPGA_PATH}/tmp/${test_name}
+    # (cd ${OPENFPGA_PATH}/tmp/${test_name} && ${OPENFPGA_PATH}/build/openfpga/openfpga -batch -x "${command};")
+    (cd ${OPENFPGA_PATH}/tmp/${test_name} && ${OPENFPGA_PATH}/build/openfpga/openfpga -batch -x "${command};" 2>&1 > /dev/null)
     if [ $? -ne 0 ]; then
         echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        echo "Command '${command}' failed in OpenFPGA Shell with VPR8"
+        echo "Test '${test_name}' failed"
         echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    else
+        echo "Test '${test_name}' passed"
     fi
 }
 
-cwd=$(pwd)
-rm -rf tmp
-mkdir -p ${cwd}/tmp/base
-cd ${cwd}/tmp/base
+rm -rf ${OPENFPGA_PATH}/tmp
 
-# echo -e "Testing app options commands in OpenFPGA Shell with VPR8"
+echo -e "Testing app options commands"
 
-# run_openfpga_command "show_vpr_setup;"
+# Test 1: show_vpr_setup
+run_tcl_command "test1" "show_vpr_setup;"
 
-# run_openfpga_command "\
-# set_app_option -n atom.const_gen_inference -v comb; \
-# show_vpr_setup;"
+# Test 2: report_app_option
+run_tcl_command "test2" "report_app_option -n atom.const_gen_inference;"
+
+# Test 3: set_app_option and show_vpr_setup
+run_tcl_command "test3" "
+    set_app_option -n atom.const_gen_inference -v comb_seq;
+    report_app_option -n atom.const_gen_inference;
+    show_vpr_setup;"
+
+# Test 4: run a complete flow from openfpga shell
+run_tcl_command "test4" "
+    read_vpr_arch -f ${OPENFPGA_PATH}/openfpga_flow/vpr_arch/k4_frac_N4_tileable_40nm_custrrg.xml -l FPGA44_CUSTOM_GRAPH;
+    read_circuit -f ${OPENFPGA_PATH}/openfpga_flow/benchmarks/micro_benchmark/and2/and2.blif;
+    pack;
+    place;
+    route;" | tee openfpga_shell_output.log
 
 
-run_openfpga_command "
-vpr ${OPENFPGA_PATH}/openfpga_flow/vpr_arch/k4_frac_N4_tileable_40nm_custrrg.xml \
-${OPENFPGA_PATH}/openfpga_flow/benchmarks/micro_benchmark/and2/and2.blif \
---device FPGA44_CUSTOM_GRAPH --route_chan_width 100" | tee openfpga_shell_output1.log
+# Test 5: run a complete flow from openfpga shell
+run_tcl_command "test5" "
+    read_vpr_arch -f ${OPENFPGA_PATH}/openfpga_flow/vpr_arch/k4_frac_N4_tileable_40nm_custrrg.xml -l FPGA44_CUSTOM_GRAPH;
+    read_circuit -f ${OPENFPGA_PATH}/openfpga_flow/benchmarks/micro_benchmark/and2/and2.blif;
+    pack;
+    place;
+    set_app_option -n router.flat_routing -v true;
+    route;" | tee openfpga_shell_output.log
 
 
-mkdir -p ${cwd}/tmp/exp
-cd ${cwd}/tmp/exp
-
-run_openfpga_command "\
-read_vpr_arch -f ${OPENFPGA_PATH}/openfpga_flow/vpr_arch/k4_frac_N4_tileable_40nm_custrrg.xml -l 4x4;
-read_circuit -f ${OPENFPGA_PATH}/openfpga_flow/benchmarks/micro_benchmark/and2/and2.blif;
-pack;place;route;" | tee openfpga_shell_output.log
+# run_tcl_command "
+#     vpr ${OPENFPGA_PATH}/openfpga_flow/vpr_arch/k4_frac_N4_tileable_40nm_custrrg.xml \
+#     ${OPENFPGA_PATH}/openfpga_flow/benchmarks/micro_benchmark/and2/and2.blif \
+#     --device FPGA44_CUSTOM_GRAPH --route_chan_width 100" | tee openfpga_shell_output1.log
