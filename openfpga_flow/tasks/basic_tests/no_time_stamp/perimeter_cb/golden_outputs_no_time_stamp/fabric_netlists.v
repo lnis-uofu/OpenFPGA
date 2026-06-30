4d84e74c3e8c8a81949b8da7ad6cf420842523fe
tangxifan <tangxifan@gmail.com>
2026-06-29 17:11:33 -0700

    [test] update arch for abspath

--- openfpga_flow/tasks/basic_tests/no_time_stamp/perimeter_cb/golden_outputs_no_time_stamp/fabric_netlists.v
+++ openfpga_flow/tasks/basic_tests/no_time_stamp/perimeter_cb/golden_outputs_no_time_stamp/fabric_netlists.v
@@ -8,8 +8,8 @@
 `include "fpga_defines.v"
 
 // ------ Include user-defined netlists -----
-`include "dff.v"
-`include "gpio.v"
+`include "../../../../../openfpga_cell_library/verilog/dff.v"
+`include "../../../../../openfpga_cell_library/verilog/gpio.v"
 // ------ Include primitive module netlists -----
 `include "sub_module/inv_buf_passgate.v"
 `include "sub_module/arch_encoder.v"
