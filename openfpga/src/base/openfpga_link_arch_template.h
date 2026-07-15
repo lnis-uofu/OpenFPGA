79af393b094c2d6d3bef0ac904b9621fa57bf21a
Amin Mohaghegh <amohaghegh@quicklogic.com>
2026-07-15 13:58:50 -0700

    fix warning

--- openfpga/src/base/openfpga_link_arch_template.h
+++ openfpga/src/base/openfpga_link_arch_template.h
@@ -125,7 +125,7 @@ int link_arch_template(T& openfpga_ctx, const Command& cmd,
   annotate_device_rr_gsb(
     g_vpr_ctx.device(), openfpga_ctx.mutable_device_rr_gsb(),
     !openfpga_ctx.clock_arch().empty(), /* FIXME: consider to be more robust! */
-    in_edges, g_vpr_ctx.device().gsb_version,
+    in_edges, gsb_version,
     cmd_context.option_enable(cmd, opt_allow_gsb_dangling_opin),
     cmd_context.option_enable(cmd, opt_verbose));
 
