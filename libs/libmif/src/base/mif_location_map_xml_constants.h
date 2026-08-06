232c43b724f807f7ed0d1c271308436b3acb1b9d
treelin611 <jrlin@rapid-flex.com>
2026-08-06 20:19:43 +0800

    reformat

---
+++ libs/libmif/src/base/mif_location_map_xml_constants.h
@@ -0,0 +1,16 @@
+#ifndef MIF_LOCATION_MAP_XML_CONSTANTS_H
+#define MIF_LOCATION_MAP_XML_CONSTANTS_H
+
+/* Constants required by MIF location map XML writer */
+
+constexpr const char* XML_MIF_COORDINATES_ROOT_NAME = "mif_coordinates";
+constexpr const char* XML_MIF_LOCATION_NODE_NAME = "mif";
+constexpr const char* XML_MIF_LOCATION_ATTRIBUTE_PB_TYPE = "pb_type";
+constexpr const char* XML_MIF_LOCATION_ATTRIBUTE_PORT = "port";
+constexpr const char* XML_MIF_LOCATION_ATTRIBUTE_DATA_OFFSET = "data_offset";
+constexpr const char* XML_MIF_LOCATION_ATTRIBUTE_DATA_WIDTH = "data_width";
+constexpr const char* XML_MIF_LOCATION_ATTRIBUTE_X = "x";
+constexpr const char* XML_MIF_LOCATION_ATTRIBUTE_Y = "y";
+constexpr const char* XML_MIF_LOCATION_ATTRIBUTE_Z = "z";
+
+#endif
