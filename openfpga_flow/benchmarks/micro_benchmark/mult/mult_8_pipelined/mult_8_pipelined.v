57fd2b0bb28480112a3d581a5d6a7d420bd06da2
tangxifan <tangxifan@gmail.com>
2026-08-07 13:28:33 -0700

    New testcase to validate Atom block constraints and Bus-based routing (#2680)
    * [test] add a new testcase to validate fix atom block and bus routing
    
    * [test] add new arch
    
    * [test] update vpr constraints
    
    * [test] update blif
    
    * [test] use a new benchmark mult_8_pipelined
    
    * [test] fixed the bug in dsp reg
    
    * [test] enable vpr constraints. Look like when a mult registers are partially used, it can be applied bus routing, unless yosys dff is added to output constant 0 or 1

---
+++ openfpga_flow/benchmarks/micro_benchmark/mult/mult_8_pipelined/mult_8_pipelined.v
@@ -0,0 +1,32 @@
+//-------------------------------------------------------
+//  Functionality: A 8-bit multiply circuit with pipelines
+//  Author:        Xifan Tang
+//-------------------------------------------------------
+
+module mult_8_pipelined(clk, a, b, out);
+parameter DATA_WIDTH = 8;  /* declare a parameter. default required */
+input [0: DATA_WIDTH - 1] a, b;
+input clk;
+output [0: DATA_WIDTH * 2 - 1] out;
+
+reg [0 : DATA_WIDTH - 1] a_reg;
+reg [0 : DATA_WIDTH - 1] b_reg;
+reg [0 : DATA_WIDTH * 2 - 1] out_reg;
+
+always @(posedge clk) begin
+  a_reg <= a;
+  b_reg <= b;
+  out_reg <= a_reg * b_reg;
+end
+
+assign out = out_reg;
+endmodule
+
+
+
+
+
+
+
+
+
