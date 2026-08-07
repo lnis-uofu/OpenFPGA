038c1fe86df90afa9dbb8449cb671f2e8e56e08a
tangxifan <tangxifan@gmail.com>
2026-08-07 11:25:35 -0700

    Use Yosys as a CMake submodule (#2672)
    * [core] update yosys to latest which has cmake
    
    * [core] simplify cmake when yosys supports cmake
    
    * [core] now add yosys cmake module path
    
    * [core] debug cmake syntax for yosys-lang
    
    * [core] debug
    
    * [lib] update yosys to latest
    
    * [lib] remove yosys-slang as it is now part of yosys
    
    * [core] remove slang from cmake
    
    * [core] use a custom yosys for cmake test
    
    * [lib] disable unit tests in yosys
    
    * [core] use a custom yosys
    
    * [core] use a custom yosys cmake
    
    * [core] update yosys
    
    * [lib] update custom yosys
    
    * [core] update yosys
    
    * [core] update yosys
    
    * [core] update yosys
    
    * [core] update yosys
    
    * [core] updaste yosys
    
    * [core] update yosys
    
    * [core] update yosys to latest
    
    * [core] update yosys to latest
    
    * [lib] update yosys to latest
    
    * [lib] update yosys
    
    * [lib] update yosys to latest
    
    * [lib] update yosys
    
    * [test] update yosys bin path as it follows cmake paths
    
    * [core] switch to https url for yosys submodule
    
    * [ci] add missing deps: gtest to windows build. Now use Gcc-12 as minimum requirements
    
    * [ci] clean up gcc version on ubuntu 22.04
    
    * [ci] rename
    
    * [doc] now gcc-12 is the minimum requirements
    
    * [core] remove the out-of-dated yosys build scripts
    
    * [ci] debug win build errors, gcc-12 enable on centos 9 and reg tests
    
    * [ci] debug docker creation
    
    * [ci] debug docker reg tests
    
    * [core] suppress cmake yosys warnings
    
    * [core] add yosys abc to warning suppress
    
    * [lib] update ys to latest
    
    * [lib] update yosys
    
    * [core] disable vpr server mode to check if sockpp static lib issues remain
    
    * [ci] disable yosys build on msys2
    
    * [core] remove custom yosys install command as it is now managed by cmake
    
    * [core] suppress compiler warnings for yosys libs
    
    * [core] fix the bug on docker image build
    
    * [core] debug yosys subtargets in cmake
    
    * [core] exclude yosys kernel from warnings
    
    * [ci] add share directory to artifacts
    
    * [core] now apply warning suppression on all the yosys targets
    
    * [ci] debug file tree in reg test jobs
    
    * [ci] now no warning build remains on gcc-11 without Yosys; Disable Yosys in msvc build; Update Yosys binary path for artifacts
    
    * [lib] update yosys to latest
    
    * [test] update yosys paths for msvc and artifacts
    
    * [core] update yosys
    
    * [ci] debug msvc build
    
    * [core] update yosys
    
    * [core] update yosys
    
    * [core] update yosys
    
    * [core] update yosys
    
    * [ci] enable yosys build on msvc
    
    * [ci] correct share directory in artifacts
    
    * [core] Now slang command is part of native yosys build. No need to load a plugin
    
    * [ci] debug msvc build
    
    * [ci] typo
    
    * [ci] debug msvc build for yosys
    
    * [ci] debug
    
    * [ci] restore cmake build for msvc

--- .github/workflows/build_dependency_win-msys2-MINGW64.sh
+++ .github/workflows/build_dependency_win-msys2-MINGW64.sh
@@ -65,6 +65,7 @@ pacman -S --noconfirm --needed \
     mingw-w64-x86_64-qt-installer-framework \
     mingw-w64-x86_64-capnproto \
     mingw-w64-x86_64-wineditline \
+    mingw-w64-x86_64-gtest \
     mingw-w64-x86_64-openssl
 
 # This is for openfpga regression tests
