389a8d9def17df653e3d050ed347c3bc9cf18e25
treelin611 <jrlin@rapid-flex.com>
2026-08-14 13:49:03 +0800

    fix ci error

--- libs/libmif/test/test_aggregate_mif.cpp
+++ libs/libmif/test/test_aggregate_mif.cpp
@@ -1,89 +1,81 @@
 /********************************************************************
- * Unit test: Yosys eblif .param INIT -> MifPipeline
+ * Unit test: read MIF settings and an init.hex file.
  *
  * Usage:
- *   test_aggregate_mif <bitstream_setting.xml> <yosys_output.eblif>
+ *   test_aggregate_mif <bitstream_setting.xml> <init.hex>
  *******************************************************************/
 #include <cstdint>
-#include <map>
 #include <string>
-#include <vector>
 
 #include "bitstream_setting.h"
 #include "command_exit_codes.h"
-#include "mif_pipeline.h"
-#include "mif_storage_fwd.h"
+#include "mif_storage.h"
+#include "read_mif.h"
 #include "read_xml_openfpga_arch.h"
-#include "vpr_context.h"
 #include "vtr_log.h"
-#include "write_mif.h"
-
-namespace {
-
-/* Stub resolver: unit test has no packed VPR netlist. */
-const std::vector<std::string> k_stub_pb_types = {
-  "memory[mem_8x16_dp].mem_8x16_dp[0]", "memory[mem_8x16_dp].mem_8x16_dp[1]"};
-size_t g_next_stub_pb_type = 0;
-
-std::string stub_mif_pb_type_from_vpr(
-  const AtomContext&, const DeviceContext&, const std::string&,
-  const openfpga::MifEblifPortConnections&) {
-  return k_stub_pb_types.at(g_next_stub_pb_type++);
-}
-
-} /* namespace */
 
 int main(int argc, const char** argv) {
   if (argc != 3) {
-    VTR_LOG_ERROR("Usage: %s <bitstream_setting.xml> <yosys_output.eblif>\n",
-                  argv[0]);
+    VTR_LOG_ERROR("Usage: %s <bitstream_setting.xml> <init.hex>\n", argv[0]);
     return openfpga::CMD_EXEC_FATAL_ERROR;
   }
 
   openfpga::BitstreamSetting bitstream_setting;
-  int status = read_xml_openfpga_bitstream_settings(argv[1], bitstream_setting);
-  if (openfpga::CMD_EXEC_SUCCESS != status) {
-    return status;
+  const int setting_status =
+    read_xml_openfpga_bitstream_settings(argv[1], bitstream_setting);
+  if (openfpga::CMD_EXEC_SUCCESS != setting_status) {
+    return setting_status;
   }
 
-  openfpga::MifPipeline pipeline;
-  AtomContext atom_ctx;
-  DeviceContext device_ctx;
-  status = pipeline.load_eblif(argv[2], bitstream_setting, atom_ctx, device_ctx,
-                               stub_mif_pb_type_from_vpr);
-  if (openfpga::CMD_EXEC_SUCCESS != status) {
-    return status;
+  size_t num_mif_sources = 0;
+  std::string pb_type;
+  for (const MifSourceSettingId& source_id :
+       bitstream_setting.mif_source_settings()) {
+    if (pb_type.empty()) {
+      pb_type = bitstream_setting.mif_source_pb_type(source_id);
+    }
+    ++num_mif_sources;
   }
-  status = pipeline.merge_to_logical(bitstream_setting);
-  if (openfpga::CMD_EXEC_SUCCESS != status) {
-    return status;
+  if (num_mif_sources == 0 || pb_type.empty()) {
+    VTR_LOG_ERROR("No MIF source was read from bitstream settings\n");
+    return openfpga::CMD_EXEC_FATAL_ERROR;
   }
-  status = pipeline.decode_logical(bitstream_setting);
-  if (openfpga::CMD_EXEC_SUCCESS != status) {
-    return status;
-  }
-  status = pipeline.aggregate_to_physical(bitstream_setting);
-  if (openfpga::CMD_EXEC_SUCCESS != status) {
-    return status;
-  }
-  const MifSegmentId out_segment(0);
 
-  std::map<uint64_t, std::string> words;
+  openfpga::MifStorage mif_storage;
+  const int mif_status =
+    openfpga::read_mif_from_init_hex(argv[2], mif_storage, pb_type);
+  if (openfpga::CMD_EXEC_SUCCESS != mif_status) {
+    return mif_status;
+  }
+  if (mif_storage.num_segments() != 1) {
+    VTR_LOG_ERROR("Expected one MIF segment, found %zu\n",
+                  mif_storage.num_segments());
+    return openfpga::CMD_EXEC_FATAL_ERROR;
+  }
+
+  const MifSegmentId segment_id(0);
+  if (mif_storage.physical_pb(segment_id) != pb_type) {
+    VTR_LOG_ERROR("MIF segment pb_type does not match bitstream settings\n");
+    return openfpga::CMD_EXEC_FATAL_ERROR;
+  }
+
+  size_t num_words = 0;
+  uint64_t expected_address = 0;
   for (const MifMemoryLineId& line_id :
-       pipeline.storage(openfpga::MifPipeline::Stage::PHYSICAL)
-         .segment_memory_lines(out_segment)) {
-    words[pipeline.storage(openfpga::MifPipeline::Stage::PHYSICAL)
-            .memory_line_address(line_id)] =
-      pipeline.storage(openfpga::MifPipeline::Stage::PHYSICAL)
-        .memory_line_data(line_id);
+       mif_storage.segment_memory_lines(segment_id)) {
+    if (mif_storage.memory_line_address(line_id) != expected_address ||
+        mif_storage.memory_line_data(line_id).size() != 16) {
+      VTR_LOG_ERROR("Unexpected MIF word at index %zu\n", num_words);
+      return openfpga::CMD_EXEC_FATAL_ERROR;
+    }
+    ++num_words;
+    ++expected_address;
+  }
+  if (num_words != 8) {
+    VTR_LOG_ERROR("Expected 8 MIF words, found %zu\n", num_words);
+    return openfpga::CMD_EXEC_FATAL_ERROR;
   }
 
-  status = pipeline.write_stage("aggregated_preload.mem",
-                                openfpga::MifPipeline::Stage::PHYSICAL);
-
-  if (openfpga::CMD_EXEC_SUCCESS != status) {
-    return status;
-  }
-  VTR_LOG("MifPipeline eblif-source test passed.\n");
+  VTR_LOG("Bitstream-setting and read_mif tests passed.\n");
   return openfpga::CMD_EXEC_SUCCESS;
 }
