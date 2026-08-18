06767963cfe0ab56af034cda38f36b656e171574
tangxifan <tangxifan@gmail.com>
2026-08-18 09:02:31 -0700

    Remove all the non-gz golden copies  (#2700)
    * [test] dev testcase for atom block location fix
    
    * [test] remove non-gz copy generation from no time stamp scripts
    
    * [test] update golden
    
    * [test] use gz for frac_dsp_mux_golden copies
    
    * [test] update gz output script
    
    * [test] update golden

--- openfpga_flow/tasks/basic_tests/no_time_stamp/device_1x1/golden_outputs_no_time_stamp/fpga_defines.v
+++
@@ -1,8 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//-------------------------------------------
-`define ENABLE_TIMING 1
-
--- openfpga_flow/tasks/basic_tests/no_time_stamp/perimeter_cb/golden_outputs_no_time_stamp/sub_module/local_encoder.v
+++
@@ -1,6 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Local Decoders for Multiplexers
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//-------------------------------------------
