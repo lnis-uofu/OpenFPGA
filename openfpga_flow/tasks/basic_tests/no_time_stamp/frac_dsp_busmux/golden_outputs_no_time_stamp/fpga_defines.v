1afcd47ac1ff3e6dddb75b6153b45b1836995749
tangxifan <tangxifan@gmail.com>
2026-08-17 17:32:03 -0700

    [test] update golden

--- openfpga_flow/tasks/basic_tests/no_time_stamp/frac_dsp_busmux/golden_outputs_no_time_stamp/fpga_defines.v
+++
@@ -1,6 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//-------------------------------------------
