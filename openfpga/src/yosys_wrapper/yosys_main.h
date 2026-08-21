78a261d00b02ca3c036c9995951931cf67c15ce2
tangxifan <tangxifan@gmail.com>
2026-08-20 17:01:22 -0700

    Now Yosys can be called through API in OpenFPGA Shell (#2706)
    * [test] dev testcase for atom block location fix
    
    * [core] try link yosys as static library
    
    * [core] add yosys command using API calls directly
    
    * [core] syntax
    
    * [core] syntax
    
    * [core] debug yosys log and share dir name
    
    * [core] now copy file after openfpga is built
    
    * [core] add yosys dependencies  on openfpga build
    
    * [core] typo
    
    * [core] code format
    
    * [core] add compile macro to disable yosys
    
    * [test] add new testcase to validate built-in yosys command
    
    * [test] debug
    
    * [doc] add yosys command
    
    * [core] try dependency on libopenfpga and yosys
    
    * [core] remove compiler warning
    
    * [core] remove more flags
    
    * [core] syntax
    
    * [core] debug build errors on clean build due to misorder yosys and openfpga subdirectories
    
    * [ci] debug missing files
    
    * [ci] add libyosys shared lib to ld path on ci machine
    
    * [ci] debug yosys-abc executable

---
+++ openfpga/src/yosys_wrapper/yosys_main.h
@@ -0,0 +1,14 @@
+#ifdef OPENFPGA_INCLUDE_YOSYS_COMMAND
+
+#pragma once
+
+#include <string>
+
+/* Begin namespace openfpga */
+namespace openfpga {
+
+int yosys_script_mode_wrapper(const std::string& script_path);
+
+} /* End namespace openfpga */
+
+#endif
