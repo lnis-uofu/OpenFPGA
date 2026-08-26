b4475808ab6a83a98cad1865e78de12228f6422f
treelin611 <jrlin@rapid-flex.com>
2026-08-26 15:13:16 +0800

    mod code according to comments

--- openfpga_flow/scripts/run_fpga_flow.py
+++ openfpga_flow/scripts/run_fpga_flow.py
@@ -1067,7 +1067,7 @@ def run_openfpga_shell():
         path_variables["VPR_TESTBENCH_BLIF"] = normalize_template_path_for_windows(yosys_eblif)
 
     # write_mif --file; override via task.conf --openfpga_write_mif_file
-    path_variables.setdefault("OPENFPGA_WRITE_MIF_FILE", "fabric_independent_bitstream_memory.mem")
+    path_variables.setdefault("OPENFPGA_WRITE_MIF_FILE", "ram_mif.mem")
     path_variables["OPENFPGA_WRITE_MIF_FILE"] = normalize_template_path_for_windows(
         path_variables["OPENFPGA_WRITE_MIF_FILE"]
     )
