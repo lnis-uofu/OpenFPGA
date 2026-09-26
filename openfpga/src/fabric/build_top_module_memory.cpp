49a1ee169bdd551085bd1167ef4fc3545d708b32
tangxifan <tangxifan@gmail.com>
2026-09-25 17:46:29 -0700

    [core] update for macos which deprecated the random_shuffle

--- openfpga/src/fabric/build_top_module_memory.cpp
+++ openfpga/src/fabric/build_top_module_memory.cpp
@@ -568,6 +568,9 @@ void shuffle_top_module_configurable_children(
 #ifdef _WIN32
   static std::mt19937 rng(std::random_device{}());
   std::shuffle(shuffled_keys.begin(), shuffled_keys.end(), rng);
+#elif defined(__APPLE__) 
+  static std::mt19937 rng(std::random_device{}());
+  std::shuffle(shuffled_keys.begin(), shuffled_keys.end(), rng);
 #else
   std::random_shuffle(shuffled_keys.begin(), shuffled_keys.end());
 #endif
